2171bf793d6c09a507aa62079f81d5b9801eb265
sioodmy <sioodmy@tuta.io>
2022-02-18 14:54:50 +0100

    Update picom configuration

--- config/picom.nix
+++ config/picom.nix
@@ -3,15 +3,24 @@
 {
     services.picom = {
         enable = true;
-        shadow = false;
-        shadowOpacity = "0.3";
+        shadow = true;
+        shadowOpacity = "0.2";
         extraOptions = ''
             shadow-radius = 10;
-            corner-radius = 10;
+            corner-radius = 5;
             round-borders = 1;
         '';
+        fadeExclude = [
+          "class_g = 'Rofi'"
+          "class_g = 'slop'"
+        ];
+        shadowExclude = [
+          "class_g = 'slop'"
+        ];
+
         fade = true;
+        fadeDelta = 4;
+        fadeSteps = [ "0.03" "0.03" ];
         vSync = true;
-        fadeDelta = 5;
     };
 }
