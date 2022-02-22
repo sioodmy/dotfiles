747ba3b20919ad4c94f65d31dceb3c8c0827441d
sioodmy <sioodmy@tuta.io>
2022-02-22 20:59:30 +0100

    Add gcc

--- packages.nix
+++ packages.nix
@@ -9,19 +9,23 @@
         # cli tools
         exa fzf ffmpeg unzip xclip bat pfetch
         libnotify gnupg update-nix-fetchgit yt-dlp
-        ripgrep rsync ncmpcpp mpd tldr imagemagick
-        scrot maim lolcat bottom
+        ripgrep rsync ncmpcpp mpd mpc_cli imagemagick
+        scrot maim bottom playerctl newsboat
+        tealdeer cava
         # gui apps
         ungoogled-chromium obs-studio mpv sxiv 
-        qbittorrent pavucontrol
-        # unfree apps 
-        discord minecraft
+        transmission-gtk pavucontrol 
+        # unfree apps (sorry daddy stallman)
+        discord minecraft 
         # dev tools
-        python3 rustup git jdk dconf
+        python3 rustup git jdk dconf gcc
         # language servers for nvim
         rnix-lsp
 
-        (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        # fonts
+        source-sans
+        twemoji-color-font
+        (nerdfonts.override { fonts = [ "JetBrainsMono" "FiraCode"]; })
 
     ];
 }
