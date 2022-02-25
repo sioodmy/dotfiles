9ff9659e9ffe646b4eae5784612089cd82a142d3
sioodmy <sioodmy@tuta.io>
2022-02-25 17:49:07 +0100

    Improve hosts

--- config/cursor.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, config, ...}:
-
-{
-    home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
-}
---
+++ system/hosts/graphene.nix
@@ -0,0 +1,4 @@
+{ pkgs, ...}: 
+{
+  networking.hostName = "graphene";
+}
