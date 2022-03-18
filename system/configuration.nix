f77d53bab57fcd275f140f80e20bceaa59cf6802
sioodmy <sioodmy@tuta.io>
2022-03-18 16:39:09 +0100

    🧹 Cleanup

--- system/configuration.nix
+++ system/configuration.nix
@@ -5,33 +5,32 @@ with lib;
 let 
   theme = import ../theme;
 in
-{
+  {
     environment.variables = {
-        NIXOS_CONFIG="$HOME/.config/nixos/configuration.nix";
-        NIXOS_CONFIG_DIR="$HOME/.config/nixos/";
-        EDITOR="nvim";
-        TERMINAL="alacritty";
-        BROWSER="firefox";
+      NIXOS_CONFIG="$HOME/.config/nixos/configuration.nix";
+      NIXOS_CONFIG_DIR="$HOME/.config/nixos/";
+      EDITOR="nvim";
+      TERMINAL="alacritty";
+      BROWSER="firefox";
     };
 
     nix = {
-        autoOptimiseStore = true;
-        allowedUsers = [ "sioodmy" ];
-        gc = {
-            automatic = true;
-            dates = "daily";
-        };
-        package = pkgs.nixUnstable;
-        extraOptions = ''
+      autoOptimiseStore = true;
+      allowedUsers = [ "sioodmy" ];
+      gc = {
+        automatic = true;
+        dates = "daily";
+      };
+      package = pkgs.nixUnstable;
+      extraOptions = ''
             experimental-features = nix-command flakes
-        '';
+      '';
     };
 
     hardware = {
       opengl.driSupport32Bit = true;
       pulseaudio.support32Bit = true;
       nvidia.modesetting.enable = true;
-
     };
 
     environment.defaultPackages = [ ];
@@ -42,7 +41,7 @@ in
       plymouth.enable = true;
       kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
       kernelPackages = pkgs.linuxPackages_latest;
-#      kernelPackages = pkgs.linuxPackages_xanmod;
+      # kernelPackages = pkgs.linuxPackages_xanmod;
       consoleLogLevel = 0;
       initrd.verbose = false;
       loader = {
@@ -59,8 +58,6 @@ in
             rev = "3f62cd4174465631b40269a7c5631e5ee86dec45";
             sha256 = "d15FS7R78kdUKqC7EAei5Pe0Vuj2boVnm4WZYQdPURo=";
           } + "/catppuccin-grub-theme";
-
-
         };
       };
     };
@@ -75,9 +72,9 @@ in
     };
 
     environment.etc."libinput-gestures.conf".text = ''
-    gesture swipe right 3 bspc desktop -f next.local
-    gesture swipe left 3 bspc desktop -f prev.local
-     '';
+      gesture swipe right 3 bspc desktop -f next.local
+      gesture swipe left 3 bspc desktop -f prev.local
+    '';
 
     sound.enable = true;
 
@@ -154,101 +151,96 @@ in
         };
       };
 
-
-
-    # enable and secure ssh
-    openssh = { 
-      enable = true;
-      permitRootLogin = "no";
-      passwordAuthentication = false;
-    };
-
-    # Use pipewire instead of soyaudio
-    pipewire = {
-      enable = true;
-      alsa = {
+      # enable and secure ssh
+      openssh = { 
         enable = true;
-        support32Bit = true;
+        permitRootLogin = "no";
+        passwordAuthentication = false;
       };
-      pulse.enable = true;
-      jack.enable = true;
+
+      # Use pipewire instead of soyaudio
+      pipewire = {
+        enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+        pulse.enable = true;
+        jack.enable = true;
+      };
     };
-  };
 
+    users.users.sioodmy = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      shell = pkgs.zsh;
+    };
 
-  users.users.sioodmy = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
-  };
+    fonts.fonts = with pkgs; [
+      jetbrains-mono 
+      roboto
+      source-sans
+      twitter-color-emoji
+      inter
+      iosevka
+      (nerdfonts.override { fonts = [ "JetBrainsMono" "Iosevka" ]; })
+    ];
 
-  fonts.fonts = with pkgs; [
-    jetbrains-mono 
-    roboto
-    source-sans
-    twitter-color-emoji
-    inter
-    iosevka
-    (nerdfonts.override { fonts = [ "JetBrainsMono" "Iosevka" ]; })
-  ];
-
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = false;
-
+    system.autoUpgrade = {
+      enable = true;
+      dates = "daily";
+      allowReboot = false;
+    };
 
     # Security 
-
     boot.blacklistedKernelModules = [
-        # Obscure network protocols
-        "ax25"
-        "netrom"
-        "rose"
-        # Old or rare or insufficiently audited filesystems
-        "adfs"
-        "affs"
-        "bfs"
-        "befs"
-        "cramfs"
-        "efs"
-        "erofs"
-        "exofs"
-        "freevxfs"
-        "f2fs"
-        "hfs"
-        "hpfs"
-        "jfs"
-        "minix"
-        "nilfs2"
-        "omfs"
-        "qnx4"
-        "qnx6"
-        "sysv"
-        "ufs"
-      ]; 
+      # Obscure network protocols
+      "ax25"
+      "netrom"
+      "rose"
+      # Old or rare or insufficiently audited filesystems
+      "adfs"
+      "affs"
+      "bfs"
+      "befs"
+      "cramfs"
+      "efs"
+      "erofs"
+      "exofs"
+      "freevxfs"
+      "f2fs"
+      "hfs"
+      "hpfs"
+      "jfs"
+      "minix"
+      "nilfs2"
+      "omfs"
+      "qnx4"
+      "qnx6"
+      "sysv"
+      "ufs"
+    ]; 
 
-      boot.kernel.sysctl = {
-        "kernel.yama.ptrace_scope" = mkOverride 500 1;
-        "kernel.kptr_restrict" = mkOverride 500 2;
-        "net.core.bpf_jit_enable" = mkDefault false;
-        "kernel.ftrace_enabled" = mkDefault false;
-        "net.ipv4.conf.all.log_martians" = mkDefault true;
-        "net.ipv4.conf.all.rp_filter" = mkDefault "1";
-        "net.ipv4.conf.default.log_martians" = mkDefault true;
-        "net.ipv4.conf.default.rp_filter" = mkDefault "1";
-        "net.ipv4.icmp_echo_ignore_broadcasts" = mkDefault true;
-        "net.ipv4.conf.all.accept_redirects" = mkDefault false;
-        "net.ipv4.conf.all.secure_redirects" = mkDefault false;
-        "net.ipv4.conf.default.accept_redirects" = mkDefault false;
-        "net.ipv4.conf.default.secure_redirects" = mkDefault false;
-        "net.ipv6.conf.all.accept_redirects" = mkDefault false;
-        "net.ipv6.conf.default.accept_redirects" = mkDefault false;
-        "net.ipv4.conf.all.send_redirects" = mkDefault false;
-        "net.ipv4.conf.default.send_redirects" = mkDefault false;
-      };
+    boot.kernel.sysctl = {
+      "kernel.yama.ptrace_scope" = mkOverride 500 1;
+      "kernel.kptr_restrict" = mkOverride 500 2;
+      "net.core.bpf_jit_enable" = mkDefault false;
+      "kernel.ftrace_enabled" = mkDefault false;
+      "net.ipv4.conf.all.log_martians" = mkDefault true;
+      "net.ipv4.conf.all.rp_filter" = mkDefault "1";
+      "net.ipv4.conf.default.log_martians" = mkDefault true;
+      "net.ipv4.conf.default.rp_filter" = mkDefault "1";
+      "net.ipv4.icmp_echo_ignore_broadcasts" = mkDefault true;
+      "net.ipv4.conf.all.accept_redirects" = mkDefault false;
+      "net.ipv4.conf.all.secure_redirects" = mkDefault false;
+      "net.ipv4.conf.default.accept_redirects" = mkDefault false;
+      "net.ipv4.conf.default.secure_redirects" = mkDefault false;
+      "net.ipv6.conf.all.accept_redirects" = mkDefault false;
+      "net.ipv6.conf.default.accept_redirects" = mkDefault false;
+      "net.ipv4.conf.all.send_redirects" = mkDefault false;
+      "net.ipv4.conf.default.send_redirects" = mkDefault false;
+    };
 
-
-      security.protectKernelImage = true;
-      system.stateVersion = "21.11"; # DONT TOUCH THIS 
-
-    }
-
+    security.protectKernelImage = true;
+    system.stateVersion = "21.11"; # DONT TOUCH THIS 
+  }
