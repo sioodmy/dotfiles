c818cd22ce5cc3dd6479b31913892505891f4d87
sioodmy <sioodmy@tuta.io>
2022-03-20 20:42:39 +0100

    🎨 Add wallpaper
    Former-commit-id: 2f3c0b18a6c6942acfc4c0a22eb545f1e0299423

--- home/desktop/bspwm/default.nix
+++ home/desktop/bspwm/default.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, theme, ... }:
+{ config, pkgs, theme, fetchurl, ... }:
 
 {
   home.packages = with pkgs; [
     feh
     bspswallow
   ];
+
+  home.file.".local/backgrounds/wallpaper.png".source = ../../../theme/wallpaper.png;
 
   xsession = {
     enable = true;
@@ -16,7 +18,7 @@
     windowManager.bspwm = {
       enable = true;
       startupPrograms = [
-          "feh --bg-fill ~/.local/backgrounds/* --randomize"
+          "feh --bg-fill ~/.local/backgrounds/wallpaper.png" 
           "xrandr --output DP-0 --mode 1920x1080 --rate 150"
           "pgrep bspswallow || bspswallow" 
           "xsetroot -cursor_name left_ptr"
