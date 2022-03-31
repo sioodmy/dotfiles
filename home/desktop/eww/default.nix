48719a1c7489cd34493a22d0b2ab6f2858a136d8
sioodmy <sioodmy@tuta.io>
2022-03-31 22:23:09 +0200

    🚧 Add eww bar
    Former-commit-id: 908eee50f6e9fe7d5e570bccaad3a738d697f769

--- home/desktop/eww/default.nix
+++ home/desktop/eww/default.nix
@@ -1,4 +1,44 @@
-{ config, theme, pkgs, ...}:
+{ inputs, config, theme, pkgs, ...}:
 
 {
+  # eww package
+  home.packages = [ inputs.eww.packages."x86_64-linux".eww pkgs.pamixer ];
+
+  # configuration
+  home.file.".config/eww/eww.yuck".source = ./eww.yuck;
+
+  # color definitions
+  home.file.".config/eww/eww.scss".text = with theme.colors; "
+$accent: #${ac};
+$background: #${bg};
+
+$black: #${ba};
+$red: #${c1};
+$green: #${c2};
+$yellow: #${c3};
+$blue: #${c4};
+$magenta: #${c5};
+$white: $foreground;
+  " + builtins.readFile ./eww.scss;
+
+  # scripts
+  home.file.".config/eww/scripts/memory.sh" = {
+    source = ./scripts/memory.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/music.sh" = {
+    source = ./scripts/music.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/workspaces.sh" = {
+    source = ./scripts/workspaces.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/microphone_icon.sh" = {
+    source = ./scripts/microphone_icon.sh;
+    executable = true;
+  };
 }
