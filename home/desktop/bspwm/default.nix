dba2ee28c48d12fc98ec622177127c462140d0c5
sioodmy <sioodmy@tuta.io>
2022-04-01 21:35:11 +0200

    😀 Improve eww bar

--- home/desktop/bspwm/default.nix
+++ home/desktop/bspwm/default.nix
@@ -27,7 +27,7 @@
         remove_unplugged_monitors = true;
         ignore_ewmh_focus = true;
         focus_follows_pointer = true;
-        border_width = 2;
+        border_width = 1;
         window_gap = 6;
         top_padding = 50;
         focused_border_color = "#${ac}";
