3e2fad699ab26d5bc6bf25809849bd849647cce3
sioodmy <sioodmy@tuta.io>
2022-04-05 21:46:35 +0200

    feat: add todo

--- home/packages.nix
+++ home/packages.nix
@@ -1,20 +1,22 @@
-{ pkgs, config, ... }:
+{ inputs, pkgs, config, ... }:
 
 {
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
+    inputs.todo.packages."${system}".todo
+    maim 
     # cli tools
     exa ffmpeg unzip xclip fetch
     libnotify gnupg update-nix-fetchgit yt-dlp ytmdl
     ripgrep rsync imagemagick
     scrot bottom newsboat unrar
-    tealdeer cava killall onefetch
-    todo du-dust
+    tealdeer killall onefetch
+    du-dust 
     # gui apps
     obs-studio mpv sxiv tdesktop
     transmission-gtk pavucontrol pcmanfm
     # unfree apps (sorry daddy stallman)
-    discord minecraft steam
+    minecraft steam
     # dev tools
     python3 git jdk dconf gcc rustc rustfmt cargo
   ];
