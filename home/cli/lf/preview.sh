ea523a269b2852ca5902b365fb46cb48354b5c81
sioodmy <sioodmy@tuta.io>
2022-04-09 12:22:34 +0200

    feat: lf file previewer

---
+++ home/cli/lf/preview.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *) bat --color always "$1";;
+esac
+
