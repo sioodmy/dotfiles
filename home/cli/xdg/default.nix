888d7fb91b1f0e9161aa59c92196f017fa53c8c9
sioodmy <sioodmy@tuta.io>
2022-04-10 13:00:35 +0200

    feat: xdg mime

--- home/cli/xdg/default.nix
+++ home/cli/xdg/default.nix
@@ -1,16 +1,30 @@
 { pkgs, config, ... }:
-
+let
+  defaults = {
+        "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+        "application/x-bittorrent" = [ "transmission-gtk.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+  };
+in
 {
-
-  xdg.userDirs = {
-    enable = true;
-    documents = "$HOME/other";
-    download = "$HOME/download";
-    videos = "$HOME/vids";
-    music = "$HOME/music";
-    pictures = "$HOME/pics";
-    desktop = "$HOME/other";
-    publicShare = "$HOME/other";
-    templates = "$HOME/other";
+  xdg = {
+    userDirs = {
+      enable = true;
+      documents = "$HOME/other";
+      download = "$HOME/download";
+      videos = "$HOME/vids";
+      music = "$HOME/music";
+      pictures = "$HOME/pics";
+      desktop = "$HOME/other";
+      publicShare = "$HOME/other";
+      templates = "$HOME/other";
+    };
+    mimeApps = {
+      enable = true;
+      associations.added = defaults;
+      defaultApplications = defaults;
+    };
   };
 }
+
