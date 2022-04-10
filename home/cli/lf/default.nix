4ad805988fc2ffffadcdaff39e56284ec2887c83
sioodmy <sioodmy@tuta.io>
2022-04-10 13:01:41 +0200

    feat: update lf configuration

--- home/cli/lf/default.nix
+++ home/cli/lf/default.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     ffmpegthumbnailer
     poppler_utils
+    atool
     bat
     p7zip
     unzip
@@ -14,6 +15,7 @@
     odt2txt
     wkhtmltopdf
     imagemagick
+    trash-cli
   ];
   home.file.".config/lf/cleaner.sh" = {
     source = ./cleaner.sh;
@@ -24,8 +26,9 @@
     enable = true;
     settings = {
       tabstop = 4;
-      ratios = "1:2:1.5";
+      ratios = "1:3:3";
       promptfmt = "";
+      scrolloff = 10;
       icons = true;
       dirfirst = true;
       drawbox = false;
@@ -37,7 +40,6 @@
 
     extraConfig = ''
       set cleaner ~/.config/lf/cleaner.sh
-      &[[ -n "$ZLE_FIFO" ]] && lf -remote "send $id zle-init"
     '';
 
     keybindings = 
@@ -45,24 +47,42 @@
       gh = "cd ~"; 
       gd = "cd ~/download";
       gv = "cd ~/vids";
+      x = "execute";
       gm = "cd ~/music";
-      au = "unarchive";
-#      "." = "set hidden!";
-      "." = "zle-cd";
+      au = "extract";
+      D = "%trash -i $f";
+      "." = "set hidden!";
       a = "zle-insert-relative";
       A = "zle-insert-absolute";
-      o = "&xdg-open $f";
+      o = "open";
+      e = "$$EDITOR $f";
+      sh = "share";
+      Y = "yank-path";
     };
 
      commands = {
-      open = ''
-        ''${{
-          case $(${ pkgs.file }/bin/file --mime-type $f -b) in
-            text/* | */lfrc ) $EDITOR $fx;;
-            *) for f in $fx; do devour xdg-open $f > /dev/null 2> /dev/null & done;;
-          esac
-        }}
-      '';
+       open = "&devour xdg-open \"$fx\"";
+       execute = "$time $f && read -r line";
+       mkdir = "%IFS=\" \"; mkdir -- \"$*\"";
+       yank-path = "$printf '%s' \"$fx\" | xclip -i -selection clipboard && notify-send \"Files\" \"Path copied to clipboard!\"";
+       share = "$curl -F\"file=@$fx\" https://0x0.st | xclip -selection c && notify-send \"Files\" \"Link copied to clipboard\"";
+       extract = "$\{{
+	clear; tput cup $(($(tput lines)/3)); tput bold
+	set -f
+	printf \"%s\\n\\t\" \"$fx\"
+	printf \"extract?[y/N]\"
+	read ans
+	[ $ans = \"y\" ] && aunpack $fx
+}}
+       ";
+
+       newfold = "$\{{
+    set -f
+    read newd
+    printf \"Directory name: \"
+    mkdir -- \"$newd\"
+    mv -- $fx \"$newd\"
+}}";
     };
   };
 
