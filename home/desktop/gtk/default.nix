963314c5b7da7bbe08f58bd55099ea05d71d047f
sioodmy <sioodmy@tuta.io>
2022-04-15 12:48:36 +0200

    feat(gtk): convert to module

--- home/desktop/gtk/default.nix
+++ home/desktop/gtk/default.nix
@@ -1,19 +1,24 @@
-{ pkgs, config, fetchzip, ... }:
+{ pkgs, lib, config, fetchzip, ... }:
+with lib;
+let cfg = config.modules.desktop.gtk;
+in {
+  options.modules.desktop.gtk = { enable = mkEnableOption "gtk"; };
 
-{
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-green-dark-compact";
-      package = pkgs.catppuccin-gtk;
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    font = {
-      name = "Inter";
-      size = 12;
+  config = mkIf cfg.enable {
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Catppuccin-green-dark-compact";
+        package = pkgs.catppuccin-gtk;
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+      font = {
+        name = "Inter";
+        size = 12;
+      };
     };
   };
 }
