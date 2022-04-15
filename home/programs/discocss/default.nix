adcf985b03d6a80c0345438ffe552568fe737888
sioodmy <sioodmy@tuta.io>
2022-04-15 12:50:11 +0200

    feat(discocss): convert to module
    Former-commit-id: b446944f666a1c1790c90f6a5f80d8fd9ee5d6ee

--- home/programs/discocss/default.nix
+++ home/programs/discocss/default.nix
@@ -1,5 +1,5 @@
-{ pkgs, config, ... }:
-
+{ pkgs, lib, config, ... }:
+with lib;
 let
   wrapped = pkgs.writeShellScriptBin "Discord" ''
     exec ${pkgs.discocss}/bin/discocss
@@ -8,18 +8,24 @@ let
     name = "discord";
     paths = [ wrapped pkgs.discord ];
   };
+  cfg = config.modules.programs.discocss;
 
 in {
-  programs.discocss = {
-    enable = true;
-    discordAlias = false;
-    discord = discord-css;
-    css = builtins.readFile "${pkgs.fetchurl {
-      url =
-        "https://raw.githubusercontent.com/catppuccin/discord/0576dcc2dece84f47894003e8f10fa15eb2db661/main.css";
-      sha256 = "sha256-EFDIs4SOPlJpxc25vhzDJ660r84vrM/20aVzE4fpo/A=";
-    }}";
-  };
+  options.modules.programs.discocss = { enable = mkEnableOption "discocss"; };
 
-  home.packages = [ discord-css ];
+  config = mkIf cfg.enable {
+    programs.discocss = {
+      enable = true;
+      discordAlias = false;
+      discord = discord-css;
+      css = builtins.readFile "${pkgs.fetchurl {
+        url =
+          "https://raw.githubusercontent.com/catppuccin/discord/0576dcc2dece84f47894003e8f10fa15eb2db661/main.css";
+        sha256 = "sha256-EFDIs4SOPlJpxc25vhzDJ660r84vrM/20aVzE4fpo/A=";
+      }}";
+    };
+
+    home.packages = [ discord-css ];
+  };
 }
+
