fd06833ee09795726555ba8b384f16d208ad692b
sioodmy <sioodmy@tuta.io>
2022-04-15 12:51:56 +0200

    feat(udiskie): convert to module

--- home/services/udiskie/default.nix
+++ home/services/udiskie/default.nix
@@ -1,9 +1,14 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
+with lib;
+let cfg = config.modules.services.udiskie;
+in {
+  options.modules.services.udiskie = { enable = mkEnableOption "udiskie"; };
 
-{
-  services.udiskie = {
-    enable = true;
-    automount = true;
-    notify = true;
+  config = mkIf cfg.enable {
+    services.udiskie = {
+      enable = true;
+      automount = true;
+      notify = true;
+    };
   };
 }
