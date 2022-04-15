a4b793cd8aaa3a4359b885fa9fe6a7b99c95b344
sioodmy <sioodmy@tuta.io>
2022-04-15 12:51:11 +0200

    feat(urxvt): convert to module
    Former-commit-id: f3e67d8291d29a75c7a6574999c971106a09c1cf

--- home/programs/urxvt/default.nix
+++ home/programs/urxvt/default.nix
@@ -1,15 +1,22 @@
-{ pkgs, config, theme, ... }: {
-  programs.urxvt = with theme.colors; {
-    enable = true;
-    fonts = [ "xft:monospace:size=15,xft:Twitter Color Emoji:size=15" ];
-    scroll = {
-      bar.enable = false;
-      keepPosition = true;
-      lines = 10000;
-      scrollOnKeystroke = true;
-      scrollOnOutput = false;
+{ pkgs, lib, config, theme, ... }:
+with lib;
+let cfg = config.modules.programs.urxvt;
+in {
+  options.modules.programs.urxvt = { enable = mkEnableOption "urxvt"; };
+
+  config = mkIf cfg.enable {
+    programs.urxvt = with theme.colors; {
+      enable = true;
+      fonts = [ "xft:monospace:size=15,xft:Twitter Color Emoji:size=15" ];
+      scroll = {
+        bar.enable = false;
+        keepPosition = true;
+        lines = 10000;
+        scrollOnKeystroke = true;
+        scrollOnOutput = false;
+      };
+      shading = 88;
+      extraConfig = { "internalBorder" = 24; };
     };
-    shading = 88;
-    extraConfig = { "internalBorder" = 24; };
   };
 }
