49e3d8d04d0ca9f5aa8b88535c3ce20b65752564
sioodmy <sioodmy@tuta.io>
2022-04-15 12:49:07 +0200

    feat(alacritty): convert to module

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -1,113 +1,118 @@
-{ pkgs, config, theme, ... }:
+{ pkgs, lib, config, theme, ... }:
+with lib;
+let cfg = config.modules.programs.alacritty;
+in {
+  options.modules.programs.alacritty = { enable = mkEnableOption "alacritty"; };
 
-{
-  home.packages = [ pkgs.tdrop ];
-  programs.alacritty = {
-    enable = true;
-    settings = with theme.colors; {
-      key_bindings = [
-        {
-          key = "C";
-          mods = "Control|Alt";
-          action = "Copy";
-        }
-        {
-          key = "V";
-          mods = "Control|Alt";
-          action = "Paste";
-        }
-        {
-          key = "Equals";
-          mods = "Command";
-          action = "IncreaseFontSize";
-        }
-        {
-          key = "Minus";
-          mods = "Command";
-          action = "DecreaseFontSize";
-        }
-        {
-          key = "Minus";
-          mods = "Command|Shift";
-          action = "ResetFontSize";
-        }
-      ];
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.tdrop ];
+    programs.alacritty = {
+      enable = true;
+      settings = with theme.colors; {
+        key_bindings = [
+          {
+            key = "C";
+            mods = "Control|Alt";
+            action = "Copy";
+          }
+          {
+            key = "V";
+            mods = "Control|Alt";
+            action = "Paste";
+          }
+          {
+            key = "Equals";
+            mods = "Command";
+            action = "IncreaseFontSize";
+          }
+          {
+            key = "Minus";
+            mods = "Command";
+            action = "DecreaseFontSize";
+          }
+          {
+            key = "Minus";
+            mods = "Command|Shift";
+            action = "ResetFontSize";
+          }
+        ];
 
-      dynamic_padding = true;
+        dynamic_padding = true;
 
-      window = {
-        dimensions = {
-          columns = 100;
-          lines = 25;
+        window = {
+          dimensions = {
+            columns = 100;
+            lines = 25;
+          };
+          padding = {
+            x = 16;
+            y = 16;
+          };
         };
-        padding = {
-          x = 16;
-          y = 16;
+        scrolling = {
+          history = 38;
+          multiplier = 3;
         };
-      };
-      scrolling = {
-        history = 38;
-        multiplier = 3;
-      };
-      font = {
-        size = 12;
-        normal = {
-          family = "monospace";
-          style = "Medium";
-        };
-        bold = {
-          family = "monospace";
-          style = "Bold";
-        };
-        italic = {
-          family = "monospace";
-          style = "Light italic";
-        };
-        use_thin_strokes = true;
-        offset = {
-          x = 0;
-          y = 0;
-        };
-        glyph_offset = {
-          x = 0;
-          y = 0;
-        };
-      };
-
-      mouse.hide_when_typing = true;
-
-      colors = {
-        primary = {
-          background = "0x${bg}";
-          foreground = "0x${fg}";
-        };
-        cursor = {
-          text = "0x${bg}";
-          cursor = "0x${c6}";
+        font = {
+          size = 12;
+          normal = {
+            family = "monospace";
+            style = "Medium";
+          };
+          bold = {
+            family = "monospace";
+            style = "Bold";
+          };
+          italic = {
+            family = "monospace";
+            style = "Light italic";
+          };
+          use_thin_strokes = true;
+          offset = {
+            x = 0;
+            y = 0;
+          };
+          glyph_offset = {
+            x = 0;
+            y = 0;
+          };
         };
 
-        normal = {
-          black = "0x${c0}";
-          red = "0x${c1}";
-          green = "0x${c2}";
-          yellow = "0x${c3}";
-          blue = "0x${c4}";
-          magenta = "0x${c6}";
-          cyan = "0x${c5}";
-          white = "0x${fg}";
-        };
+        mouse.hide_when_typing = true;
 
-        bright = {
-          black = "0x${c8}";
-          red = "0x${c9}";
-          green = "0x${c10}";
-          yellow = "0x${c11}";
-          blue = "0x${c12}";
-          magenta = "0x${c14}";
-          cyan = "0x${c13}";
-          white = "0x${fg}";
-        };
+        colors = {
+          primary = {
+            background = "0x${bg}";
+            foreground = "0x${fg}";
+          };
+          cursor = {
+            text = "0x${bg}";
+            cursor = "0x${c6}";
+          };
 
+          normal = {
+            black = "0x${c0}";
+            red = "0x${c1}";
+            green = "0x${c2}";
+            yellow = "0x${c3}";
+            blue = "0x${c4}";
+            magenta = "0x${c6}";
+            cyan = "0x${c5}";
+            white = "0x${fg}";
+          };
+
+          bright = {
+            black = "0x${c8}";
+            red = "0x${c9}";
+            green = "0x${c10}";
+            yellow = "0x${c11}";
+            blue = "0x${c12}";
+            magenta = "0x${c14}";
+            cyan = "0x${c13}";
+            white = "0x${fg}";
+          };
+
+        };
       };
     };
   };
