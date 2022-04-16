95f269c32691f0fe82d2604bb125f6175389955f
sioodmy <sioodmy@tuta.io>
2022-04-16 18:46:27 +0200

    feat: add lockscreen module
    Former-commit-id: ea8b74173db30aee2d29e2c52c00f2d03cae6009

---
+++ modules/home/desktop/lockscreen/default.nix
@@ -0,0 +1,104 @@
+{ config, pkgs, lib, theme, ... }:
+with lib;
+let
+  cfg = config.modules.desktop.lockscreen;
+  sleep-noti = pkgs.writeScript "sleep-noti" ''
+    #! ${pkgs.bash}/bin/bash
+    ${pkgs.libnotify}/bin/notify-send 'Sleeping in 60 seconds..'
+  '';
+  lockscreen = (with theme.colors;
+    pkgs.writeScriptBin "lockscreen" ''
+      #! ${pkgs.zsh}/bin/zsh
+      background=#${bg}
+      insidecolor=#${bg}ff
+      insidevercolor=#${bg}ff
+      insidewrongcolor=#${c1}ff
+      ringcolor=#${ac}1A
+      ringvercolor=#${c4}ff
+      ringwrongcolor=#${c1}ff
+      keyhlcolor=#${c2}ff
+      bshlcolor=#${c1}ff
+      layoutcolor=#${ac}ff
+      seperatorcolor=#${ba}00
+      timecolor=#${fg}7f
+      datecolor=#${fg}ff
+      verifcolor=#${fg}ff
+      wrongcolor=#${bg}ff
+      ${pkgs.i3lock-color}/bin/i3lock-color \
+        -n \
+        --indicator \
+        -c $background \
+        -S=0 \
+        --inside-color=$insidecolor \
+        --insidever-color=$insidevercolor \
+        --insidewrong-color=$insidewrongcolor \
+        \
+        --ring-color=$ringcolor \
+        --ringver-color=$ringvercolor \
+        --ringwrong-color=$ringwrongcolor \
+        \
+        --line-uses-inside \
+        --keyhl-color=$keyhlcolor \
+        --bshl-color=$bshlcolor \
+        \
+        --layout-color=$layoutcolor \
+        --separator-color=$seperatorcolor \
+        --radius=160 \
+        --ring-width=24 \
+        --noinput-text="Enter Password" \
+        \
+        --clock \
+        \
+        --time-color=$timecolor \
+        --time-str="%H:%M:%S" \
+        --time-pos="x+w/2:y+h/1.8" \
+        --time-font="monospace" \
+        --time-size=22 \
+        \
+        --date-color=$datecolor \
+        --date-str="%A" \
+        --date-pos="x+w/2:y+h/1.95" \
+        --date-font="monospace" \
+        --date-size=57 \
+        \
+        --verif-text="" \
+        --verif-color=$verifcolor \
+        --verif-font="monospace" \
+        --verif-pos="x+w/2:y+h/2" \
+        --verif-size=30 \
+        \
+        --wrong-text="" \
+        --wrong-color=$wrongcolor \
+        --wrong-font="monospace" \
+        --wrong-pos="x+w/2:y+h/2" \
+        --wrong-size=30 &&\
+    '');
+in {
+
+  options.modules.desktop.lockscreen = {
+    enable = mkEnableOption "lockscreen";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.i3lock-color lockscreen ];
+
+    services.screen-locker = {
+      enable = true;
+      inactiveInterval = 10;
+      xautolock.enable = true;
+      xautolock.extraOptions = [
+        "-corners '--00'"
+        "-cornersize 20"
+        "-notify 60"
+        "-notifier ${sleep-noti}"
+      ];
+      lockCmd = "${lockscreen}/bin/lockscreen";
+    };
+
+    services.sxhkd.keybindings = mkIf config.modules.services.sxhkd.enable {
+      "super + shift + p" = "${lockscreen}/bin/lockscreen";
+    };
+
+  };
+
+}
