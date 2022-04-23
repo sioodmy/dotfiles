1f0bbc4d9de66167854ff6e63b338d9446ced41f
sioodmy <sioodmy@tuta.io>
2022-04-23 23:57:52 +0200

    feat: add thinkpad host configuration
    Former-commit-id: 4ed193a3a2f244a3e398cd9a823c8636fa7f0207

---
+++ hosts/thinkpad/user.nix
@@ -0,0 +1,41 @@
+{ config, lib, inputs, ...}:
+
+{
+  imports = [ ../../modules/home/default.nix ];
+  config.modules = {
+    desktop = {
+      bspwm.enable = true;
+      eww.enable = true;
+      gtk.enable = true;
+      picom.enable = true;
+      xresources.enable = true;
+      lockscreen.enable = true;
+    };
+    programs = {
+      discocss.enable = true;
+      firefox.enable = true;
+      qutebrowser.enable = true;
+      kitty.enable = true;
+      zathura.enable = true;
+      rofi.enable = true;
+    };
+    services = {
+      sxhkd.enable = true;
+      redshift.enable = true;
+      udiskie.enable = true;
+    };
+    cli = {
+      nvim.enable = true;
+      bat.enable = true;
+      btm.enable = true;
+      cava.enable = true;
+      fzf.enable = true;
+      git.enable = true;
+      music.enable = true;
+      zsh.enable = true;
+      gpg.enable = true;
+      lf.enable = true;
+      xdg.enable = true;
+    };
+  };
+}
