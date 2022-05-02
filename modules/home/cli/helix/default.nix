2068d54067694e734e51549834bf4a5fcb5a988d
sioodmy <sioodmy@tuta.io>
2022-05-02 23:07:45 +0200

    feat: update helix configuration

--- modules/home/cli/helix/default.nix
+++ modules/home/cli/helix/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, theme, ... }:
 with lib;
 let cfg = config.modules.cli.helix;
 in {
@@ -8,69 +8,103 @@ in {
     programs.helix = {
       enable = true;
       settings = {
-          theme = "base16";
-          lsp.display-messages = true;
-          keys.normal = {
-            space.space = "file_picker";
-            space.w = ":w";
-            space.q = ":q";
-          };
-      };
-      themes = {
-          base16 = let
-            transparent = "none";
-            gray = "#665c54";
-            dark-gray = "#3c3836";
-            white = "#fbf1c7";
-            black = "#282828";
-            red = "#fb4934";
-            green = "#b8bb26";
-            yellow = "#fabd2f";
-            orange = "#fe8019";
-            blue = "#83a598";
-            magenta = "#d3869b";
-            cyan = "#8ec07c";
-          in {
-            "ui.menu" = transparent;
-            "ui.menu.selected" = { modifiers = [ "reversed" ]; };
-            "ui.linenr" = { fg = gray; bg = dark-gray; };
-            "ui.popup" = { modifiers = [ "reversed" ]; };
-            "ui.linenr.selected" = { fg = white; bg = black; modifiers = [ "bold" ]; };
-            "ui.selection" = { fg = black; bg = blue; };
-            "ui.selection.primary" = { modifiers = [ "reversed" ]; };
-            "comment" = { fg = gray; };
-            "ui.statusline" = { fg = white; bg = dark-gray; };
-            "ui.statusline.inactive" = { fg = dark-gray; bg = white; };
-            "ui.help" = { fg = dark-gray; bg = white; };
-            "ui.cursor" = { modifiers = [ "reversed" ]; };
-            "variable" = red;
-            "variable.builtin" = orange;
-            "constant.numeric" = orange;
-            "constant" = orange;
-            "attributes" = yellow;
-            "type" = yellow;
-            "ui.cursor.match" = { fg = yellow; modifiers = [ "underlined" ]; };
-            "string" = green;
-            "variable.other.member" = red;
-            "constant.character.escape" = cyan;
-            "function" = blue;
-            "constructor" = blue;
-            "special" = blue;
-            "keyword" = magenta;
-            "label" = magenta;
-            "namespace" = blue;
-            "diff.plus" = green;
-            "diff.delta" = yellow;
-            "diff.minus" = red;
-            "diagnostic" = { modifiers = [ "underlined" ]; };
-            "ui.gutter" = { bg = black; };
-            "info" = blue;
-            "hint" = dark-gray;
-            "debug" = dark-gray;
-            "warning" = yellow;
-            "error" = red;
+        lsp.display-messages = true;
+        editor = {
+          search.smart-case = true;
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
           };
         };
+        theme = "base16";
+      };
+      themes =  {
+          base16 = with theme.colors; let
+            background = "#${bg}";
+            foreground = "#${fg}";
+            transparent = "none";
+            gray = "#${c8}";
+            dark-gray = "#${c0}";
+            white = "#${c7}";
+            black = "#${ba}";
+            red = "#${c1}";
+            light-red = "${c9}";
+            green = "#${c2}";
+            yellow = "#${c3}";
+            orange = "#${c10}";
+            blue = "#${c4}";
+            magenta = "#${c5}";
+            pink = "${c6}";
+            cyan = "#${c12}";
+          in {
+            "atribute" = blue;
+            "keyword" = blue;
+            "keyword.directive" = red;
+            "namespace" = orange;
+            "punctation" = foreground;
+            "punctation.delimiter" = blue;
+            "operator" = blue;
+            "special" = orange;
+            "variable.other.member" = green;
+            "variable" = orange;
+            "variable.parameter" = { fg = pink; };
+            "variable.builtin" = magenta;
+            "type" = blue;
+            "type.builtin" = foreground;
+            "constructor" = blue;
+            "function" = red;
+            "function.macro" = light-red;
+            "function.builtin" = blue;
+            "tag" = orange;
+            "comment" = gray;
+            "constant" = foreground;
+            "constant.builtin" = green;
+            "string" = green;
+            "constant.numeric" = blue;
+            "constant.character.escape" = orange;
+            "label" = orange;
+
+            "markup.heading" = magenta;
+            "markup.bold" = { modifiers = ["bold"]; };
+            "markup.italic" = { modifiers = ["italic"]; };
+            "markup.link.url" = { fg = white;  modifiers = ["underlined"]; };
+            "markup.link.text" = orange;
+            "markup.raw" = orange;
+            "diff.plus" = green;
+            "diff.minus" = red;
+            "diff.delta" = magenta;
+            
+            "ui.background" = { bg = background;};
+            "ui.linenr" = { fg = dark-gray; };
+            "ui.linenr.selected" = { fg = magenta; };
+            "ui.statusline" = { fg = foreground; bg = black; };
+            "ui.statusline.inactive" = { fg = magenta; bg = gray; };
+            "ui.popup" = { bg = black; };
+            "ui.window" = { fg = light-red; };
+            "ui.help" = { bg = black; fg = foreground; };
+            "ui.text" = { fg = pink; };
+            "ui.text.focus" = { fg = white; };
+            "ui.selection" = { bg = black; };
+            "ui.selection.primary" = { bg = black; };
+
+            "ui.cursor.select" = { bg = magenta; };
+            "ui.cursor.insert" = { bg = blue; };
+            "ui.cursor.match" = { fg = foreground; bg = green; };
+            "ui.cursor" = { modifiers = ["reversed"]; };
+            "ui.highlight" = { bg = light-red; };
+
+            "ui.menu.selected" = { fg = green; bg = black; };
+
+            
+            "diagnostic" = { modifiers = ["underlined"]; };
+
+            "warning" = yellow;
+            "error" = red;
+            "info" = blue;
+            "hint" = orange;
+          };
+      };
     };
-  };
-}
+    };
+ } 
