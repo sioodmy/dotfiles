e1589ccf3e90830388ed037f8b956afe40530d61
sioodmy <sioodmy@tuta.io>
2022-05-23 22:29:43 +0200

    feat(nvim): new cmp sources

--- modules/home/cli/nvim/lua/plugins/cmp.lua
+++ modules/home/cli/nvim/lua/plugins/cmp.lua
@@ -115,6 +115,9 @@ local options = {
      { name = "buffer" },
      { name = "nvim_lua" },
      { name = "path" },
+     { name = "emoji"},
+     { name = "spell"},
+     { name = "latex_symbols" },
    },
  }
 
