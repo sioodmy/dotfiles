4d8750963b572b543e441a9cfd7094e10afab2f7
sioodmy <sioodmy@tuta.io>
2022-05-27 15:47:30 +0200

    feat: add ungoogled-chromium configuration

---
+++ modules/home/programs/chromium/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let cfg = config.modules.programs.chromium;
+in {
+  options.modules.programs.chromium = { enable = mkEnableOption "chromium"; };
+
+  config = mkIf cfg.enable {
+    programs.chromium = {
+      enable = true;
+      package = pkgs.ungoogled-chromium;
+      extensions = [
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock
+        { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
+        { id = "abehfkkfjlplnjadfcjiflnejblfmmpj"; } # nord theme
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # sponsorblock
+        { id = "dhdgffkkebhmkfjojejmpbldmpobfkfo"; } # tampermonkey
+        {
+          id = "dcpihecpambacapedldabdbpakmachpb";
+          updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
+        }
+        {
+          # chromium web store
+          id = "ocaahdebbfolfmndjeplogmgcagdmblk";
+          crxPath = builtins.fetchurl {
+            name = "chromium-web-store.crx";
+            url = "https://github.com/NeverDecaf/chromium-web-store/releases/download/v1.4.0/Chromium.Web.Store.crx";
+            sha256 = "1bfzd02a9krkapkbj51kxfp4a1q5x2m2pz5kv98ywfcarbivskgs";
+          };
+          version = "1.4.0";
+        }
+        ];
+        };
+
+      };
+
+    }
