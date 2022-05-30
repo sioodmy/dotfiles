756b5b75c01e3017f2ce5a14c1223637a690bc1f
sioodmy <sioodmy@tuta.io>
2022-05-30 22:41:21 +0200

    feat: update neovim configuration

--- modules/home/cli/nvim/lua/plugins/lsp.lua
+++ modules/home/cli/nvim/lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local servers = { 'pyright', 'rnix', 'rust_analyzer', 'tsserver', 'gopls', 'ccls', 'bashls', 'cmake', 'html'}
+local servers = { 'pyright', 'rnix', 'rust_analyzer', 'tsserver', 'gopls', 'ccls', 'bashls', 'cmake', 'html', 'dartls'}
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in pairs(servers) do
@@ -67,3 +67,57 @@ saga.init_lsp_saga {
   border_style = "round",
 }
 
+local null_ls = require("null-ls")
+local prettier = require("prettier")
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+      -- format on save
+      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+    end
+
+    if client.resolved_capabilities.document_range_formatting then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
+  end,
+})
+
+prettier.setup({
+  bin = 'prettier', -- or `prettierd`
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+    "nix",
+    "rust",
+  },
+
+  -- prettier format options (you can use config files too. ex: `.prettierrc`)
+  arrow_parens = "always",
+  bracket_spacing = true,
+  embedded_language_formatting = "auto",
+  end_of_line = "lf",
+  html_whitespace_sensitivity = "css",
+  jsx_bracket_same_line = false,
+  jsx_single_quote = false,
+  print_width = 80,
+  prose_wrap = "preserve",
+  quote_props = "as-needed",
+  semi = true,
+  single_quote = false,
+  tab_width = 4,
+  trailing_comma = "es5",
+  use_tabs = false,
+  vue_indent_script_and_style = false,
+})
