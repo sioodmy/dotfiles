7fe64e6d1447e1b3a18fb3927fafb603ca1b25ff
sioodmy <sioodmy@tuta.io>
2022-05-30 22:39:21 +0200

    feat: update discord theme
    Former-commit-id: 904fa11b92dbcd253d3e33a58a11466c14bc6661

--- modules/home/programs/discocss/default.nix
+++ modules/home/programs/discocss/default.nix
@@ -1,14 +1,30 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, inputs, lib, config, ... }:
 with lib;
-let cfg = config.modules.programs.discocss;
+let
+  cfg = config.modules.programs.discocss;
+  discord-css = pkgs.symlinkJoin {
+    name = "discord";
+    paths =
+      [
+        (pkgs.writeShellScriptBin "Discord" ''
+          exec ${pkgs.discocss}/bin/discocss
+        '')
+        inputs.discord-overlay.packages."x86_64-linux".discord
+      ];
+    };
 
 in {
   options.modules.programs.discocss = { enable = mkEnableOption "discocss"; };
 
   config = mkIf cfg.enable {
 
-    home.packages = with pkgs; [ discord discocss ];
-    xdg.configFile."discocss/custom.css".source = ./nord-glasscord.theme.css;
+    programs.discocss = {
+      enable = true;
+      discordAlias = false;
+      css = builtins.readFile ./custom.css;
+    };
+
+    home.packages = [ discord-css ];
 
   };
 }
