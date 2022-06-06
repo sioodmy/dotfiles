9f6af1bf554d31036eb6d8a5c461c1870666cb3c
sioodmy <sioodmy@tuta.io>
2022-06-06 15:14:24 +0200

    feat: update alpha dashboard
    Former-commit-id: 3e0e6bf5c335fac78a25cacd14428f3826c011b4

--- modules/home/cli/nvim/lua/plugins/alpha.lua
+++ modules/home/cli/nvim/lua/plugins/alpha.lua
@@ -6,27 +6,39 @@ local dashboard = require("alpha.themes.dashboard")
 -- Set header
 
 dashboard.section.header.val = {
-   "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-   "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-   "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-   "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-   "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-   "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-   "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-   " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-   " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-   "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-   "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+  [[                                                                              ]],
+  [[                                    ██████                                    ]],
+  [[                                ████▒▒▒▒▒▒████                                ]],
+  [[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
+  [[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
+  [[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
+  [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
+  [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
+  [[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
+  [[                        ██      ██      ████      ████                        ]],
+  [[                                                                              ]],
+  [[                                                                              ]],
 }
 
+dashboard.section.header.opts = {
+  position = "center",
+  hl = "SpecialKey"
+}
 
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+  dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+  dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
+  dashboard.button( "f", "  > Live grep", ":Telescope live_grep<CR>"),
+  dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+  dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
 
 
@@ -37,5 +49,5 @@ alpha.setup(dashboard.opts)
 
 -- Disable folding on alpha buffer
 vim.cmd([[
-    autocmd FileType alpha setlocal nofoldenable
+autocmd FileType alpha setlocal nofoldenable
 ]])
