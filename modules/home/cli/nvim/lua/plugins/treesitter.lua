f5b8418b4dbc59811477672277febed897b2158a
sioodmy <sioodmy@tuta.io>
2022-06-09 22:56:02 +0200

    feaw: update neovim

--- modules/home/cli/nvim/lua/plugins/treesitter.lua
+++ modules/home/cli/nvim/lua/plugins/treesitter.lua
@@ -10,14 +10,6 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   },
 
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = {'org'},
