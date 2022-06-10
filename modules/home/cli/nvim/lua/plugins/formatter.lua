1dc49c935c3ac1614a78f56d5c45c875093906d6
sioodmy <sioodmy@tuta.io>
2022-06-10 23:52:11 +0200

    feat: add prettier support
    Former-commit-id: 2d54049cda4acc2783fd4b949f9829dc4e7616be

---
+++ modules/home/cli/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,176 @@
+local status_ok, formatter = pcall(require, "formatter")
+
+formatter.setup({
+    logging = false,
+    filetype = {
+        css = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        go = {
+            -- gofmt
+            function()
+                return { exe = "gofmt", args = { "-w", "." }, stdin = false }
+            end,
+        },
+        html = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        javascript = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        javascriptreact = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        json = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        jsonc = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        lua = {
+            -- stylua
+            function()
+                return {
+                    exe = "stylua",
+                    args = {
+                        "--call-parentheses Always",
+                        "--column-width 80",
+                        "--indent-type Spaces",
+                        "--indent-width 4",
+                        "--line-endings Unix",
+                        "--quote-style ForceDouble",
+                    },
+                    stdin = false,
+                }
+            end,
+        },
+        python = {
+            -- black
+            function()
+                return {
+                    exe = "black",
+                    args = { vim.api.nvim_buf_get_name(0) },
+                    stdin = false,
+                }
+            end,
+        },
+        rust = {
+            -- rustfmt
+            function()
+                return {
+                    exe = "rustfmt",
+                    args = { "--emit=stdout" },
+                    stdin = true,
+                }
+            end,
+        },
+        scss = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        typescript = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+        typescriptreact = {
+            -- prettier
+            function()
+                return {
+                    exe = "prettier",
+                    args = {
+                        "--stdin-filepath",
+                        vim.api.nvim_buf_get_name(0),
+                        "--tab-width 4",
+                    },
+                    stdin = true,
+                }
+            end,
+        },
+    },
+})
