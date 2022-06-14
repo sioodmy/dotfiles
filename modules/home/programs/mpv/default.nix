e842375ef5626b0ca1cbd1ff1c303a55306afebf
sioodmy <sioodmy@tuta.io>
2022-06-14 21:15:40 +0200

    feat: update mpv
    Former-commit-id: 836ba7edc83fd05188237947d08b959bf9f7ea52

--- modules/home/programs/mpv/default.nix
+++ modules/home/programs/mpv/default.nix
@@ -14,6 +14,49 @@ in {
         convert
         cutter
       ];
+      config = {
+        osc = false;
+        fullscreen = true;
+        keep-open = true;
+        force-seekable = true;
+        cursor-autohide = 100;
+        osd-bar = false;
+        audio-file-auto = "fuzzy";
+        volume = 80;
+        volume-max = 100;
+        demuxer-mkv-subtitle-preroll = true;
+        sub-font-size = 52;
+        sub-blur = 0.2;
+        sub-border-color = "0.0/0.0/0.0/1.0";
+        sub-border-size = "3.0";
+        sub-color = "1.0/1.0/1.0/1.0";
+        sub-margin-x = 100;
+        sub-margin-y = 50;
+        sub-shadow-color = "0.0/0.0/0.0/0.25";
+        sub-shadow-offset = 0;
+        alang = "ja,jp,jpn,en,eng,de,deu,ger,pol,pl";
+        slang = "en,eng,pol,pl,de,deu,ger";
+        screenshot-format = "png";
+        screenshot-sw = true;
+        screenshot-directory = "~/pics/ss";
+        screenshot-template = "%f-%wH.%wM.%wS.%wT-#%#00n";
+        correct-downscaling = true;
+        linear-downscaling = true;
+        linear-upscaling = true;
+        sigmoid-upscaling = true;
+        scale-antiring = 0.7;
+        dscale-antiring = 0.7;
+        cscale-antiring = 0.7;
+        interpolation = true;
+        tscale = "sphinx";
+        tscale-clamp = 0.0;
+        tscale-radius = 1.0;
+        tscale-blur = 0.6991556596428412;
+        gpu-api = "vulkan";
+        target-colorspace-hint = true;
+        hwdec = false;
+        dither-depth = false;
+      };
     };
   };
 }
