7755b2ecd28fab0bd00e8c356ca635470c13d7ce
sioodmy <sioodmy@tuta.io>
2022-06-21 19:55:04 +0200

    feat: update fzf theme

--- modules/home/cli/fzf/default.nix
+++ modules/home/cli/fzf/default.nix
@@ -7,24 +7,19 @@ in {
   config = mkIf cfg.enable {
     programs.zsh = {
       localVariables = {
-        FZF_DEFAULT_OPTS =
-          "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1
-          --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
-          --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-          --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'";
-          };
+        FZF_DEFAULT_OPTS = ''
+
+          --color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'';
+      };
     };
 
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-      defaultOptions = [
-        "--height 50%"
-        "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1"
-        "--color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1"
-        "--color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac"
-        "--color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'"
-      ];
+      programs.fzf = {
+        enable = true;
+        enableZshIntegration = true;
+        defaultOptions = [
+          "--height 50%"
+          "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
+        ];
+      };
     };
-  };
 }
