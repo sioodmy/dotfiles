04d8075cb6aea16039e7ee8e482b7e452ca27e9f
sioodmy <sioodmy@tuta.io>
2022-06-30 13:00:46 +0200

    feat: add f2k repo
    Former-commit-id: a7d8c23a2bac7a2082239f5bead5de65b07c21e1

--- flake.nix
+++ flake.nix
@@ -18,13 +18,14 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     discord-overlay = {
       url = "github:InternetUnexplorer/discord-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     discocss.url = "github:mlvzk/discocss/flake";
-
+    nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
     eww.url = "github:elkowar/eww";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     todo.url = "github:sioodmy/todo";
@@ -32,41 +33,38 @@
 
   };
   outputs = inputs@{ self, nixpkgs, home-manager, nur, eww
-    , discord-overlay, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-      lib = nixpkgs.lib;
+  , ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    lib = nixpkgs.lib;
 
-      mkSystem = pkgs: system: hostname:
-        pkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-            { networking.hostName = hostname; }
-            (./. + "/hosts/${hostname}/system.nix")
-            (./. + "/hosts/${hostname}/hardware-configuration.nix")
-            ./modules/system/configuration.nix
-            ./modules/system/adblock.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useUserPackages = true;
-                useGlobalPkgs = true;
-                extraSpecialArgs = { inherit inputs; };
-                users.sioodmy = (./. + "/hosts/${hostname}/user.nix");
-                sharedModules = [ inputs.discocss.hmModule ];
-              };
-              nixpkgs.overlays = [
-                (final: prev: {
-                  picom =
-                    prev.picom.overrideAttrs (o: { src = inputs.picom-dccsillag; });
-                })
-                (final: prev: {
-                  catppuccin-cursors =
-                    prev.callPackage ./overlays/catppuccin-cursors.nix { };
+    mkSystem = pkgs: system: hostname:
+    pkgs.lib.nixosSystem {
+      system = system;
+      modules = [
+        { networking.hostName = hostname; }
+        (./. + "/hosts/${hostname}/system.nix")
+        (./. + "/hosts/${hostname}/hardware-configuration.nix")
+        ./modules/system/configuration.nix
+        ./modules/system/adblock.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            extraSpecialArgs = { inherit inputs; };
+            users.sioodmy = (./. + "/hosts/${hostname}/user.nix");
+            sharedModules = [ inputs.discocss.hmModule ];
+          };
+          nixpkgs.overlays = [
+              (final: prev: {
+                catppuccin-cursors =
+                  prev.callPackage ./overlays/catppuccin-cursors.nix { };
                 })
                 nur.overlay
-                discord-overlay.overlay
+                inputs.discord-overlay.overlay
+                inputs.nixpkgs-f2k.overlay
                 inputs.neovim-nightly-overlay.overlay
               ];
             }
@@ -74,22 +72,22 @@
           ];
           specialArgs = { inherit inputs; };
         };
-    in {
-      nixosConfigurations = {
-        graphene = mkSystem inputs.nixpkgs "x86_64-linux" "graphene";
-        thinkpad = mkSystem inputs.nixpkgs "x86_64-linux" "thinkpad";
-      };
-
-      devShell.${system} = pkgs.mkShell {
-        packages = [ pkgs.nixpkgs-fmt ];
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-      };
-
-      checks.${system}.pre-commit-check =
-        inputs.pre-commit-hooks.lib.${system}.run {
-          src = self;
-          hooks.nixpkgs-fmt.enable = true;
-          hooks.shellcheck.enable = true;
-        };
+  in {
+    nixosConfigurations = {
+      graphene = mkSystem inputs.nixpkgs "x86_64-linux" "graphene";
+      thinkpad = mkSystem inputs.nixpkgs "x86_64-linux" "thinkpad";
     };
-}
+
+    devShell.${system} = pkgs.mkShell {
+      packages = [ pkgs.nixpkgs-fmt ];
+      inherit (self.checks.${system}.pre-commit-check) shellHook;
+    };
+
+    checks.${system}.pre-commit-check =
+      inputs.pre-commit-hooks.lib.${system}.run {
+        src = self;
+        hooks.nixpkgs-fmt.enable = true;
+        hooks.shellcheck.enable = true;
+      };
+    };
+  }
