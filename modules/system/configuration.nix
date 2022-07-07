e97da645d7681d61821390faef36dfb0d517ed3b
sioodmy <sioodmy@tuta.io>
2022-07-07 21:31:00 +0200

    fix(network): waiting for dhcp
    Former-commit-id: 0f1be4d07d9b61603214f7e8dc979179fed9a19c

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -71,7 +71,6 @@ with lib;
   i18n.defaultLocale = "en_US.UTF-8";
 
   networking = {
-    interfaces = { enp24s0.useDHCP = true; };
     networkmanager.enable = true;
     firewall = {
       enable = true;
