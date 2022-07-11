f787b9d153f45f2840c4d16f5690cfc79272361e
sioodmy <sioodmy@tuta.io>
2022-07-11 18:54:53 +0200

    fix(zsh): launch time fix
    Former-commit-id: b08364ac9c434dd0dc233fdc068fd14db224ef56

--- modules/home/cli/zsh/default.nix
+++ modules/home/cli/zsh/default.nix
@@ -120,13 +120,8 @@ in {
       plugins = with pkgs; [
         {
           name = "zsh-nix-shell";
+          src = pkgs.zsh-nix-shell;
           file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.5.0";
-            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-          };
         }
         {
           name = "zsh-vi-mode";
