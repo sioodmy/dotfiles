f41caae77939d29c47db25ad0014191eedaff1d1
sioodmy <sioodmy@tuta.io>
2022-07-13 21:35:59 +0200

    fix(picom): replace deprecated settings

--- modules/home/desktop/picom/default.nix
+++ modules/home/desktop/picom/default.nix
@@ -13,83 +13,80 @@ in {
 
       shadow = true;
       shadowOffsets = [ (-12) (-12) ];
-      shadowOpacity = "0.4";
-      shadowExclude = [
-        "class_g = 'slop'"
-        "class_g ?= 'peek'"
-        "_NET_WM_WINDOW_TYPE@:a *= 'SPLASH'"
-        "window_type = 'utility'"
-        "window_type = 'dropdown_menu'"
-      ];
 
       fade = true;
       fadeDelta = 5;
-      fadeSteps = [ "0.03" "0.05" ];
 
       vSync = false;
       backend = "glx";
 
-      extraOptions = ''
-      corner-radius = 10;
+      settings = {
+        fading = true;
+        fade-in-step = "0.02";
+        fade-out-step = "0.05";
+        corner-radius = 10;
+        xinerama-shadow-crop = true;
 
-      xinerama-shadow-crop = true;
-      shadow-ignore-shaped = false;
+        shadow-ignore-shaped = false;
 
-      no-fading-openclose = false
-      no-fading-destroyed-argb = true
+        no-fading-openclose = false;
+        no-fading-destroyed-argb = true;
+        active-opacity = 1.0;
+        inactive-opacity = 1.0;
+        frame-opacity = 1.0;
 
-      fade-exclude = [
-        "class_g = 'slop'"   # maim
-      ]
+        inactive-dim = 0.0;
 
-      active-opacity = 1.0;
-      inactive-opacity = 1.0;
-      frame-opacity = 1.0;
-      inactive-dim = 0.0;
+        glx-no-stencil = false;
+        glx-copy-from-front = false;
+        use-damage = true;
+        detect-rounded-corners = true;
+        detect-client-leader = true;
+        detect-transient = true;
+        unredir-if-possible = true;
 
-      opacity-rule = [];
-
-      focus-exclude = [
-            #"class_g ?= 'rofi'"
-            #'_NET_WM_NAME@:s = "rofi"'
-            "class_g ?= 'slop'",
-            "name = 'rofi'",
-            "class_g ?= 'Steam'",
-            "_NET_WM_WINDOW_TYPE@:a *= 'MENU'",
-            "window_type *= 'menu'",
-            "window_type = 'utility'",
-            "window_type = 'dropdown_menu'",
-            "window_type = 'popup_menu'"
-          ];
-          rounded-corners-exclude = [
-            "window_type = 'menu'",
-            "window_type = 'popup_menu'",
-            "window_type = 'utility'",
-          ];
-
-          glx-no-stencil = false;
-          glx-copy-from-front = false;
-          use-damage = true;
-          detect-rounded-corners = true;
-          detect-client-leader = true;
-          detect-transient = true;
-          unredir-if-possible = true;
-
-          wintypes:
-          {
-            tooltip = { fade = true; full-shadow = true; focus = true; blur-background = false;};
-            menu = { full-shadow = true;};
-            popup_menu =  { full-shadow = true;};
-            utility =  {full-shadow = true;};
-            toolbar = {full-shadow = true;};
-            normal = {full-shadow = true;};
-            notification = {full-shadow = true;};
-            dialog = {full-shadow = true};
-            dock = {full-shadow = true;};
-            dropdown_menu = { full-shadow = true;};
+        wintypes = {
+          tooltip = {
+            fade = true;
+            full-shadow = true;
+            focus = true;
+            blur-background = false;
           };
-          '';
-
+          menu = { full-shadow = true; };
+          popup_menu = { full-shadow = true; };
+          utility = { full-shadow = true; };
+          toolbar = { full-shadow = true; };
+          normal = { full-shadow = true; };
+          notification = { full-shadow = true; };
+          dialog = { full-shadow = true; };
+          dock = { full-shadow = true; };
+          dropdown_menu = { full-shadow = true; };
         };
+
+        shadowOpacity = "0.4";
+        shadowExclude = [
+          "class_g = 'slop'"
+          "class_g ?= 'peek'"
+          "_NET_WM_WINDOW_TYPE@:a *= 'SPLASH'"
+          "window_type = 'utility'"
+          "window_type = 'dropdown_menu'"
+        ];
+        focus-exclude = [
+          "class_g ?= 'slop'"
+          "name = 'rofi'"
+          "class_g ?= 'Steam'"
+          "_NET_WM_WINDOW_TYPE@:a *= 'MENU'"
+          "window_type *= 'menu'"
+          "window_type = 'utility'"
+          "window_type = 'dropdown_menu'"
+          "window_type = 'popup_menu'"
+        ];
+        rounded-corners-exclude = [
+          "window_type = 'menu'"
+          "window_type = 'popup_menu'"
+          "window_type = 'utility'"
+        ];
       };
-    }
+    };
+  };
+}
