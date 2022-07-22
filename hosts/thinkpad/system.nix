5454baf5933e09a3082da90598f3f9e01279e26f
sioodmy <sioodmy@tuta.io>
2022-07-22 21:40:52 +0200

    feat: update configuration

--- hosts/thinkpad/system.nix
+++ hosts/thinkpad/system.nix
@@ -3,23 +3,24 @@
   # undervolting and power saving stuff
   services.thermald.enable = true;
   services.upower.enable = true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "conservative";
   services.tlp = {
     enable = true;
     settings = {
       CPU_BOOST_ON_BAT = 0;
       CPU_BOOST_ON_AC = 1;
-      DISK_IDLE_SECS_ON_AC= 0;
-      DISK_IDLE_SECS_ON_BAT= 2;
-      MAX_LOST_WORK_SECS_ON_AC= 15;
-      MAX_LOST_WORK_SECS_ON_BAT= 60;
-      SCHED_POWERSAVE_ON_AC= 0;
-      SCHED_POWERSAVE_ON_BAT= 1;
+      DISK_IDLE_SECS_ON_AC = 0;
+      DISK_IDLE_SECS_ON_BAT = 2;
+      MAX_LOST_WORK_SECS_ON_AC = 15;
+      MAX_LOST_WORK_SECS_ON_BAT = 60;
+      SCHED_POWERSAVE_ON_AC = 0;
+      SCHED_POWERSAVE_ON_BAT = 1;
       START_CHARGE_THRESH_BAT0 = 70;
       STOP_CHARGE_THRESH_BAT0 = 90;
       USB_AUTOSUSPEND = 1;
-      USB_BLACKLIST_WWAN= 1;
+      USB_BLACKLIST_WWAN = 1;
       WOL_DISABLE = 1;
+      USB_AUTOSUSPEND = 0;
       USB_BLACKLIST_BTUSB = 0;
       USB_BLACKLIST_PHONE = 0;
       DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi wwan";
@@ -34,7 +35,7 @@
   hardware = {
     trackpoint = {
       emulateWheel = true;
-      speed = 140; # default: 97
+      speed = 90; # default: 97
     };
   };
   services.undervolt = {
@@ -42,6 +43,7 @@
     coreOffset = -75;
     gpuOffset = -50;
   };
-  systemd.services.undervolt.wantedBy = [ "post-resume.target" "multi-user.target" ];
+  systemd.services.undervolt.wantedBy =
+    [ "post-resume.target" "multi-user.target" ];
   systemd.services.undervolt.after = [ "post-resume.target" ];
 }
