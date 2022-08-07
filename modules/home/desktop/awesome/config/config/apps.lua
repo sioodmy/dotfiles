db157415c03c8d81030d45d057c633e276dc30a0
sioodmy <sioodmy@tuta.io>
2022-08-07 21:21:23 +0200

    cleanup: remove xorg stuff
    Former-commit-id: 1084248b58277cdc7d94440f1a85aa47240bdd61

--- modules/home/desktop/awesome/config/config/apps.lua
+++
@@ -1,5 +0,0 @@
-terminal = os.getenv("TERMINAL") or "st"
-search = "rofi -show drun"
-files = "thunar"
-editor = os.getenv("EDITOR") or "nvim"
-editor_cmd = terminal .. " -e " .. editor
--- modules/home/desktop/awesome/config/widgets/init.lua
+++
@@ -1,2 +0,0 @@
-require("widgets.bar")
-require("widgets.alttab")
