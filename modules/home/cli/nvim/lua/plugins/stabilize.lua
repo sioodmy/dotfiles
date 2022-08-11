b0a7df02d025bc5ce56bc5dfa074e11bf06c278f
sioodmy <sioodmy@tuta.io>
2022-08-11 22:39:19 +0200

    rework
    Former-commit-id: 4a8c0c8240670a905311e08d51d5f932bbe8fa62

--- modules/home/cli/nvim/lua/plugins/stabilize.lua
+++
@@ -1,7 +0,0 @@
-require("stabilize").setup{
-	force = true, -- stabilize window even when current cursor position will be hidden behind new window
-	ignore = {  -- do not manage windows matching these file/buftypes
-		filetype = { "help", "list", "Trouble" },
-		buftype = { "terminal", "quickfix", "loclist" }
-	},
-}
