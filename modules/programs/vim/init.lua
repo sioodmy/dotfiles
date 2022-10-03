db207b2fd19c05fcaf9081d27974f57be3a7c459
sioodmy <sioodmy@tuta.io>
2022-10-03 20:11:55 +0200

    update
    Former-commit-id: 6e0b5a52cc473ab2eb267a31ef85622f4a8120a4

--- modules/programs/vim/init.lua
+++ modules/programs/vim/init.lua
@@ -271,8 +271,8 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
 		{ name = "buffer" },
 		{ name = "path" },
 		{ name = "pandoc_references" },
@@ -635,3 +635,76 @@ ins_right({
 
 -- Now don't forget to initialize lualine
 lualine.setup(config)
+
+require("nvim-tree").setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_setup = false,
+	ignore_ft_on_setup = { "alpha" },
+	hijack_cursor = true,
+	hijack_unnamed_buffer_when_opening = false,
+	update_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+	},
+	view = {
+		adaptive_size = true,
+		side = "left",
+		width = 25,
+		hide_root_folder = true,
+	},
+	git = {
+		enable = false,
+		ignore = true,
+	},
+	filesystem_watchers = {
+		enable = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+	renderer = {
+		highlight_git = false,
+		highlight_opened_files = "none",
+
+		indent_markers = {
+			enable = false,
+		},
+
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = false,
+			},
+
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "",
+					symlink = "",
+					symlink_open = "",
+					arrow_open = "",
+					arrow_closed = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+})
