05e625643a4b8178bf079a0f315d0b473a60a4c1
sioodmy <sioodmy@tuta.io>
2022-11-03 19:05:47 +0100

    feat: catppuccin kvantum theme

--- home/gtk/default.nix
+++ home/gtk/default.nix
@@ -32,4 +32,17 @@
       gtk-xft-rgba="rgb"
     '';
   };
+
+  # credits: bruhvko
+  # catppuccin theme for qt-apps
+  home.packages = with pkgs; [libsForQt5.qtstyleplugin-kvantum];
+  xdg.configFile."Kvantum/catppuccin/catppuccin.kvconfig".source = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Frappe-Pink/Catppuccin-Frappe-Pink.kvconfig";
+    sha256 = "0pl936nchif2zsgzy4asrlc3gvv4fv2ln2myrqx13r6xra1vkcqs";
+  };
+  xdg.configFile."Kvantum/catppuccin/catppuccin.svg".source = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Frappe-Pink/Catppuccin-Frappe-Pink.svg";
+    sha256 = "1b92j0gb65l2pvrf90inskr507a1kwin1zy0grwcsdyjmrm5yjrv";
+  };
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = "theme=catppuccin";
 }
