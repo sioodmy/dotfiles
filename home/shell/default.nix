115fb29263b826819862d122b66b6e89951bea2c
sioodmy <sioodmy@tuta.io>
2022-11-05 21:31:52 +0100

    fix(zsh): cat alias

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -88,7 +88,7 @@ in {
       bloat = "nix path-info -Sh /run/current-system";
       ytmp3 = ''
         ${pkgs.yt-dlp}/bin/yt-dlp -x --continue --add-metadata --embed-thumbnail --audio-format mp3 --audio-quality 0 --metadata-from-title="%(artist)s - %(title)s" --prefer-ffmpeg -o "%(title)s.%(ext)s"'';
-      cat = "{pkgs.bat}/bin/bat --style=plain";
+      cat = "${pkgs.bat}/bin/bat --style=plain";
       grep = "${pkgs.ripgrep}/bin/rg";
       du = "${pkgs.du-dust}/bin/dust";
       ps = "${pkgs.procs}/bin/procs";
