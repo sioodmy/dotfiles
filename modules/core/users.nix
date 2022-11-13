d2b5212aef35a9281a7fa6144297f4663c5f4e0d
sioodmy <sioodmy@tuta.io>
2022-11-13 16:02:30 +0100

    feat: update ssh key

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -19,6 +19,6 @@
     uid = 1000;
     shell = pkgs.zsh;
     initialPassword = "changeme";
-    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfzq3529yMLJyBRswtnQixoJnKjIimbF831W6K56i7JN+XE2P+BAhena83HWOrQx/Y0KF17s2R7Ub5IiksotUIeA/UwnZUqvLWge1JEuwM6ZuTm042iGVy+IMi1zxltKnexbDkH2gc2bvcSZsl2L7jVjnykjOa+MwSG1rC8wavneGCzmEVKJmdk6kq7rCgLIH2Hr56sBpJBYtP179jT8L39nC/IxtPKtfv42OAWjp8HYKF0PFua+J2teAYSg9NPtBbogQ0LuR9nfw19g7Sj4+Um9z2QnYmp+9QIbJAzzn3eEQx8tfx0ziD/j7jk9g0FvJiP457bzz3/2Wv4jnMu54bkGV1XrjOjXT8vH7IxjxlFrKpCOZYjNZ227SPFKb6DCUGHrtZmlzjBQ0mh1d0mSVIaUmiemq/fuoSM8CJrNAkrt9ztxEh6fZ90qS4YC9UlhfUjx1C5I3osa7RNMzSqbzpiqmSmqLRiVdRVQlt1gObzVu5b37rMoM0EfoXFcJbXes= sioodmy@graphene"];
+    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9ExEl6WqtCI4yCqbSAhAGmzvVp/nYADbgy/Qi4AKQy sioodmy@anthe"];
   };
 }
