c12d1a407710e84345f2cbe2dd672fe306f302e1
sioodmy <sioodmy@tuta.io>
2022-11-16 22:56:01 +0100

    feat: newsboat rss reader
    Former-commit-id: a90e18c51807d50608d93c41240c8c895b92b952

---
+++ modules/home/newsboat/default.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.newsboat = {
+    enable = true;
+    autoReload = true;
+    urls = [
+      # https://hackaday.com/blog/feed/
+      {
+        title = "Wiadomosci blisko ciebie";
+        tags = ["news" "twitter"];
+        url = "https://nitter.net/WBC24News/rss";
+      }
+      {
+        title = "LukaszBok";
+        tags = ["news" "twitter"];
+        url = "https://nitter.net/LukaszBok/rss";
+      }
+      {
+        title = "KIKS";
+        tags = ["news" "twitter"];
+        url = "https://weekly.nixos.org/feeds/all.rss.xml";
+      }
+      {
+        title = "NixOS weekly";
+        tags = ["crypto" "twitter"];
+        url = "https://nitter.net/WatcherGuru/rss";
+      }
+    ];
+    extraConfig = ''
+      download-full-page yes
+      download-retries 3
+      error-log /var/log/newsboat-error.log
+      max-items 0
+      bind-key j down
+      bind-key k up
+      bind-key j next articlelist
+      bind-key k prev articlelist
+      bind-key J next-feed articlelist
+      bind-key K prev-feed articlelist
+      bind-key G end
+      bind-key g home
+      bind-key d pagedown
+      bind-key u pageup
+      bind-key l open
+      bind-key h quit
+      bind-key a toggle-article-read
+      bind-key n next-unread
+      bind-key N prev-unread
+      bind-key D pb-download
+      bind-key U show-urls
+      bind-key x pb-delete
+
+      color listnormal cyan default
+      color listfocus black yellow standout bold
+      color listnormal_unread blue default
+      color listfocus_unread yellow default bold
+      color info red black bold
+      color article white default bold
+    '';
+  };
+}
