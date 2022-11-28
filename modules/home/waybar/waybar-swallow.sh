b951ca1af12673ca5f499b9ffe8651bb1590a84b
sioodmy <hello@sioodmy.dev>
2022-11-28 23:05:37 +0100

    feat: move waybar to systemd user service
    Former-commit-id: 54f4725afffe34d6b2b838b9fcdf818f05fa99b7

--- modules/home/waybar/waybar-swallow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if hyprctl getoption misc:enable_swallow | rg -q "int: 1"; then
-	hyprctl keyword misc:enable_swallow false >/dev/null &&
-		notify-send "Hyprland" "Turned off swallowing"
-else
-	hyprctl keyword misc:enable_swallow true >/dev/null &&
-		notify-send "Hyprland" "Turned on swallowing"
-fi
