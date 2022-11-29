ddec604911422bdbca33ff984e7d5b86d972eebe
sioodmy <hello@sioodmy.dev>
2022-11-29 19:31:40 +0100

    fix: hyprland workspace module

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -33,7 +33,7 @@ in {
       };
       mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
       patchPhase = ''
-        substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
+        substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
       '';
     });
 
