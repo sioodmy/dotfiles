06e5db106ac48b3bf7ee8d76d7bcee1ff0b20082
sioodmy <hello@sioodmy.dev>
2022-12-03 20:21:00 +0100

    feat: update aliases

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -111,7 +111,7 @@
       };
 
       shellAliases = {
-        rebuild = "doas nix-store --verify; pushd ~dotfiles && doas nixos-rebuild --install-bootloader switch --flake .# && bat cache --build; popd";
+        rebuild = "doas nix-store --verify; pushd ~dotfiles && doas nixos-rebuild switch --flake .# && bat cache --build; popd";
         cleanup = "doas nix-collect-garbage --delete-older-than 7d";
         bloat = "nix path-info -Sh /run/current-system";
         ytmp3 = ''
