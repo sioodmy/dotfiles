18932372247017be5c371064cc05f17c2727f579
sioodmy <hello@sioodmy.dev>
2022-12-15 23:06:16 +0100

    feat: new fonts

--- modules/wayland/fonts.nix
+++ modules/wayland/fonts.nix
@@ -15,6 +15,7 @@
       comfortaa
       inter
       lato
+      lexend
       dejavu_fonts
       iosevka-bin
       noto-fonts
@@ -35,7 +36,7 @@
           "Iosevka Nerd Font"
           "Noto Color Emoji"
         ];
-        sansSerif = ["Noto Sans" "Noto Color Emoji"];
+        sansSerif = ["Lexend" "Noto Color Emoji"];
         serif = ["Noto Serif" "Noto Color Emoji"];
         emoji = ["Noto Color Emoji"];
       };
