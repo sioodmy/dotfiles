78140fabcd22724efa0a7873b39e99bd576cb34a
sioodmy <hello@sioodmy.dev>
2023-01-05 14:47:40 +0100

    feat: update flake
    Former-commit-id: 04cc4b4d6d55ef1e64046b868f2e538ce5733854

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,23 +3,13 @@
   pkgs,
   config,
   ...
-}: let
-  mpv-unwrapped = pkgs.mpv-unwrapped.overrideAttrs (o: {
-    src = pkgs.fetchFromGitHub {
-      owner = "mpv-player";
-      repo = "mpv";
-      rev = "48ad2278c7a1fc2a9f5520371188911ef044b32c";
-      sha256 = "sha256-6qbv34ysNQbI/zff6rAnVW4z6yfm2t/XL/PF7D/tjv4=";
-    };
-  });
-in {
+}: {
   nixpkgs.config.allowUnfree = false;
   home.packages = with pkgs; [
     cached-nix-shell
     todo
     brave
     libreoffice-fresh
-    mpv-unwrapped
     yt-dlp
     pavucontrol
     tdesktop
@@ -30,6 +20,7 @@ in {
     fzf
     unzip
     ripgrep
+    mpv
     rsync
     ttyper
     imagemagick
