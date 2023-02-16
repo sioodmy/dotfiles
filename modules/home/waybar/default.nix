d05a06fe7ca6189af50531976f3899a7115031e9
sioodmy <hello@sioodmy.dev>
2023-02-16 20:09:35 +0100

    feat: update waybar
    Former-commit-id: 75d923d121f3157f43397862c9a31c8b041f3023

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -29,12 +29,12 @@ in {
   xdg.configFile."waybar/style.css".text = import ./style.nix;
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-      patchPhase = ''
-        substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
-      '';
-    });
+    # package = pkgs.waybar.overrideAttrs (oldAttrs: {
+    #   # mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    #   patchPhase = ''
+    #     substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
+    #   '';
+    # });
 
     settings = {
       mainBar = {
