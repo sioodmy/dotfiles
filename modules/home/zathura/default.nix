bdaab44113d3e0b5400612ce057f4dbdeeca1618
sioodmy <hello@sioodmy.dev>
2023-03-14 16:45:20 +0100

    feat: catppuccin zathura
    Former-commit-id: a1ad0f2335ea1b15b0ac67762730e7fcb2020dd1

--- modules/home/zathura/default.nix
+++ modules/home/zathura/default.nix
@@ -9,47 +9,47 @@
     options = {
       font = "Iosevka 15";
 
-      default-fg = "#C6D0F5";
-      default-bg = "#303446";
+      default-fg = "#CDD6F4";
+      default-bg = "#1E1E2E";
 
-      completion-bg = "#414559";
-      completion-fg = "#C6D0F5";
+      completion-bg = "#313244";
+      completion-fg = "#CDD6F4";
       completion-highlight-bg = "#575268";
-      completion-highlight-fg = "#C6D0F5";
-      completion-group-bg = "#414559";
-      completion-group-fg = "#8CAAEE";
+      completion-highlight-fg = "#CDD6F4";
+      completion-group-bg = "#313244";
+      completion-group-fg = "#89B4FA";
 
-      statusbar-fg = "#C6D0F5";
-      statusbar-bg = "#414559";
+      statusbar-fg = "#CDD6F4";
+      statusbar-bg = "#313244";
       statusbar-h-padding = 10;
       statusbar-v-padding = 10;
 
-      notification-bg = "#414559";
-      notification-fg = "#C6D0F5";
-      notification-error-bg = "#414559";
-      notification-error-fg = "#E78284";
-      notification-warning-bg = "#414559";
+      notification-bg = "#313244";
+      notification-fg = "#CDD6F4";
+      notification-error-bg = "#313244";
+      notification-error-fg = "#F38BA8";
+      notification-warning-bg = "#313244";
       notification-warning-fg = "#FAE3B0";
       selection-notification = true;
 
-      inputbar-fg = "#C6D0F5";
-      inputbar-bg = "#414559";
+      inputbar-fg = "#CDD6F4";
+      inputbar-bg = "#313244";
 
       recolor = true;
-      recolor-lightcolor = "#303446";
-      recolor-darkcolor = "#C6D0F5";
+      recolor-lightcolor = "#1E1E2E";
+      recolor-darkcolor = "#CDD6F4";
 
-      index-fg = "#C6D0F5";
-      index-bg = "#303446";
-      index-active-fg = "#C6D0F5";
-      index-active-bg = "#414559";
+      index-fg = "#CDD6F4";
+      index-bg = "#1E1E2E";
+      index-active-fg = "#CDD6F4";
+      index-active-bg = "#313244";
 
-      render-loading-bg = "#303446";
-      render-loading-fg = "#C6D0F5";
+      render-loading-bg = "#1E1E2E";
+      render-loading-fg = "#CDD6F4";
 
       highlight-color = "#575268";
-      highlight-active-color = "#F4B8E4";
-      highlight-fg = "#F4B8E4";
+      highlight-active-color = "#F5C2E7";
+      highlight-fg = "#F5C2E7";
 
       selection-clipboard = "clipboard";
       adjust-open = "best-fit";
