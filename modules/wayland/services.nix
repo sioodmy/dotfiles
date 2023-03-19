7fd42b06365da905fb8853be3ca7588651614de2
sioodmy <hello@sioodmy.dev>
2023-03-19 17:28:26 +0100

    feat: move pipewire

--- modules/wayland/services.nix
+++ modules/wayland/services.nix
@@ -46,25 +46,5 @@
     udisks2.enable = true;
     printing.enable = true;
     fstrim.enable = true;
-
-    # Use pipewire instead of soyaudio
-    pipewire = {
-      enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      wireplumber.enable = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
   };
-  environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-    bluez_monitor.properties = {
-      ["bluez5.enable-sbc-xq"] = true,
-      ["bluez5.enable-msbc"] = true,
-      ["bluez5.enable-hw-volume"] = true,
-      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    }
-  '';
 }
