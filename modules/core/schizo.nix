72fb7213be2bada993bcf308362fb342a671c8c8
sioodmy <hello@sioodmy.dev>
2023-04-17 21:28:35 +0200

    feat: update bootloader config

--- modules/core/schizo.nix
+++ modules/core/schizo.nix
@@ -14,10 +14,7 @@
   };
   # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
   # on ssd systems, and volatile! Because it's wiped on reboot.
-  boot.tmpOnTmpfs = lib.mkDefault true;
-  # If not using tmpfs, which is naturally purged on reboot, we must clean it
-  # /tmp ourselves. /tmp should be volatile storage!
-  boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
+  boot.tmp.useTmpfs = lib.mkDefault true;
   # Firefox cache on tmpfs
   fileSystems."/home/sioodmy/.cache/mozilla/firefox" = {
     device = "tmpfs";
