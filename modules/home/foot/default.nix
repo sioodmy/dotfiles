b28e4686d11bccf02a5ffbcd0d21645f868741a3
sioodmy <hello@sioodmy.dev>
2023-05-03 20:38:17 +0200

    feat: update foot config

--- modules/home/foot/default.nix
+++ modules/home/foot/default.nix
@@ -14,7 +14,7 @@
     settings = {
       main = {
         term = "xterm-256color";
-        font = "monospace:size=14";
+        font = "monospace:size=13";
         vertical-letter-offset = "-0.75";
         pad = "24x24";
         dpi-aware = "yes";
