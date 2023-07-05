250bfb353114a360331a1a82f6e592c6d1f3e719
sioodmy <hello@sioodmy.dev>
2023-07-05 14:42:56 +0200

    feat: impermanence

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -96,6 +96,7 @@
         "https://hyprland.cachix.org"
         "https://webcord.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
+        "https://anyrun.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -106,6 +107,7 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "webcord.cachix.org-1:l555jqOZGHd2C9+vS8ccdh8FhqnGe8L78QrHNn+EFEs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
   };
