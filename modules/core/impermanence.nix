bdddb2764aac53e536d418dab5ba5baf6acc1b92
sioodmy <hello@sioodmy.dev>
2023-07-06 18:55:54 +0200

    feat: secureboot

--- modules/core/impermanence.nix
+++ modules/core/impermanence.nix
@@ -7,7 +7,19 @@
   imports = [inputs.impermanence.nixosModule];
   environment.persistence."/persist" = {
     hideMounts = true;
-    directories = ["/etc/nixos" "/etc/NetworkManager" "/var/log" "/var/lib" "/etc/nix" "/etc/ssh" "/var/db/sudo"];
+    directories = ["/etc/nixos" "/etc/NetworkManager" "/var/log" "/var/lib" "/etc/nix" "/etc/ssh" "/var/db/sudo" "/etc/secureboot"
+    
+    "/etc/NetworkManager/system-connections"
+       "/var/lib/flatpak"
+      "/var/lib/libvirt"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/pipewire"
+      "/var/lib/systemd/coredump"
+      "/var/cache/tailscale"
+      "/var/lib/tailscale"
+    
+    ];
     users.sioodmy = {
       files = [".database.kdbx"];
       directories = [
@@ -52,4 +64,10 @@
       ];
     };
   };
+    # for some reason *this* is what makes networkmanager not get screwed completely instead of the impermanence module
+  systemd.tmpfiles.rules = [
+    "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
+    "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
+    "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
+  ];
 }
