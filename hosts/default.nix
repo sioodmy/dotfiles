24a3b31e8cb31ba32fb890a1f3a8ecf5819e7232
sioodmy <hello@sioodmy.dev>
2023-07-07 04:22:32 +0200

    feat: cleanup
    Former-commit-id: a9b5ab32784725f8ea7fc496c421aaa5ef69a698

--- hosts/default.nix
+++ hosts/default.nix
@@ -8,7 +8,6 @@
   core = ../modules/core;
   nvidia = ../modules/nvidia;
   wayland = ../modules/wayland;
-  server = ../modules/server;
   hw = inputs.nixos-hardware.nixosModules;
   ragenix = inputs.ragenix.nixosModules.age;
   hmModule = inputs.home-manager.nixosModules.home-manager;
@@ -39,39 +38,6 @@ in {
         wayland
         hmModule
         {inherit home-manager;}
-      ]
-      ++ shared;
-    specialArgs = {inherit inputs;};
-  };
-
-  # framework laptop
-  # note that i dont actually have a framework laptop so i have no idea
-  # if this configuration works
-  io = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      [
-        {networking.hostName = "anthe";}
-        ./io/hardware-configuration.nix
-        bootloader
-        hw.framework
-        nvidia
-        wayland
-        hmModule
-        {inherit home-manager;}
-      ]
-      ++ shared;
-    specialArgs = {inherit inputs;};
-  };
-
-  # server
-  iapetus = nixpkgs.lib.nixosSystem {
-    system = "aarch64-linux";
-    modules =
-      [
-        hw.raspberry-pi-4
-        ./iapetus
-        server
       ]
       ++ shared;
     specialArgs = {inherit inputs;};
