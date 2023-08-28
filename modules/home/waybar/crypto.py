8a488892c52d3cf4993947903b4c1552df059e5e
sioodmy <hello@sioodmy.dev>
2023-08-28 14:21:17 +0200

    feat: fmt
    Former-commit-id: f7af65ea8c00e67a1c63c9a883278f3800b629b5

--- modules/home/waybar/crypto.py
+++ modules/home/waybar/crypto.py
@@ -9,19 +9,19 @@ params = {
     "order": "market_cap_desc",
     "per_page": 10,
     "price_change_percentage": "24h",
-    "page": 1
+    "page": 1,
 }
 
 response = requests.get(url, params=params)
 
 if response.status_code == 200:
     top_cryptos = response.json()
-    tooltip = "<span size=\"xx-large\">Crypto</span>\n"
+    tooltip = '<span size="xx-large">Crypto</span>\n'
     for crypto in top_cryptos:
         name = crypto["name"]
         symbol = crypto["symbol"]
         price = crypto["current_price"]
-        change= crypto["price_change_percentage_24h"]
+        change = crypto["price_change_percentage_24h"]
         tooltip += str(f"  <b>{name}</b>: ${price}  |  {change:.2f}%\n")
 
     out_data = {
