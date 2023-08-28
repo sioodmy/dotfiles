b5749ad07f2f7e2826ccb986b72ec390f38bf209
sioodmy <hello@sioodmy.dev>
2023-08-28 13:11:43 +0200

    feat: new cursor
    Former-commit-id: c508adaa014b795b57b21359724698aeaced2f45

--- flake.nix
+++ flake.nix
@@ -27,8 +27,8 @@
     nix-super.url = "github:privatevoid-net/nix-super";
 
     schizofox = {
-      url = "github:schizofox/schizofox";
-      # url = "path:/home/sioodmy/dev/schizofox";
+      # url = "github:schizofox/schizofox";
+      url = "path:/home/sioodmy/dev/schizofox";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
