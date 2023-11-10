7e924278a02da4b741b9a6947085e68686f98b55
sioodmy <hello@sioodmy.dev>
2023-11-10 17:02:22 +0100

    feat: update devshell
    Former-commit-id: 62e44cb266c65810c1b30847169d7c65c0c54530

--- devShell/default.nix
+++ devShell/default.nix
@@ -1,6 +1,18 @@
 {
   shellCommands = [
     {
+      help = "Rebuild the system using nh os switch";
+      name = "sw";
+      command = "nh os switch";
+      category = "build";
+    }
+    {
+      help = "Rebuild the system using nh os boot";
+      name = "boot";
+      command = "nh os boot";
+      category = "build";
+    }
+    {
       help = "Format the source tree with treefmt";
       name = "fmt";
       command = "treefmt";
