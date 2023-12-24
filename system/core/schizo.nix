6d6933a68016f1fab351cb10860b298061ca8149
sioodmy <hello@sioodmy.dev>
2023-12-24 15:32:36 +0100

    feat: re-enable encrypted dns

--- system/core/schizo.nix
+++ system/core/schizo.nix
@@ -7,22 +7,6 @@
       enable = true;
       allowAnyUser = true;
     };
-    dnscrypt-proxy2 = {
-      enable = false;
-      settings = {
-        ipv6_servers = true;
-        require_dnssec = true;
-
-        sources.public-resolvers = {
-          urls = [
-            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-          ];
-          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-        };
-      };
-    };
   };
   programs.ssh.startAgent = true;
   programs.wireshark.enable = true;
