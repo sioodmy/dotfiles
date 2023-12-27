02ea636b6d321c35086ad3982d9ccf16e724a084
sioodmy <hello@sioodmy.dev>
2023-12-27 22:14:01 +0100

    feat: schizofox

--- home/misc/schizofox.nix
+++ home/misc/schizofox.nix
@@ -2,17 +2,25 @@
   programs.schizofox = {
     enable = true;
     theme = {
-      background-darker = "181825";
-      background = "1e1e2e";
-      foreground = "cdd6f4";
+      colors = {
+        background-darker = "181825";
+        background = "1e1e2e";
+        foreground = "cdd6f4";
+        primary = "f5c2e7";
+      };
       font = "Lexend";
+    };
+    extensions = {
       simplefox.enable = true;
       darkreader.enable = true;
+      extraExtensions = {
+        "webextension@metamask.io".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ether-metamask/latest.xpi";
+      };
     };
-    search.searxRandomizer.enable = true;
+    search = {
+      searxUrl = "search.notashelf.dev";
+      defaultSearchEngine = "Searx";
+    };
 
-    extensions.extraExtensions = {
-      "webextension@metamask.io".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ether-metamask/latest.xpi";
-    };
   };
 }
