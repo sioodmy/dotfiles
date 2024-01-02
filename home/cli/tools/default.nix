3c4f8de24b82a11875db9360dec1af484daeae0b
sioodmy <hello@sioodmy.dev>
2024-01-02 19:20:33 +0100

    fix: deprecated options

--- home/cli/tools/default.nix
+++ home/cli/tools/default.nix
@@ -101,13 +101,18 @@ in {
       enable = true;
       config = {
         pager = "less -FR";
-        theme = "Catppuccin-mocha";
+        theme = "catppuccin-mocha";
       };
       themes = {
-        Catppuccin-mocha = builtins.readFile (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
-          hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
-        });
+        catppuccin-mocha = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-mocha.tmTheme";
+        };
       };
     };
   };
--- system/core/system.nix
+++ system/core/system.nix
@@ -100,10 +100,7 @@
     # Systemd OOMd
     # Fedora enables these options by default. See the 10-oomd-* files here:
     # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
-    oomd = {
-      enableRootSlice = true;
-      enableUserServices = true;
-    };
+    oomd.enableRootSlice = true;
 
     # TODO channels-to-flakes
     tmpfiles.rules = [
