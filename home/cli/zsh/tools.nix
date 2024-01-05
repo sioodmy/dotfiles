2145d15f6cdc3f341f3e11032bdef34a0364c940
sioodmy <hello@sioodmy.dev>
2024-01-05 23:05:14 +0100

    feat: add tools.nix
    Former-commit-id: 2a531923b1f6898a80efae67ab399dc77c46c301

---
+++ home/cli/zsh/tools.nix
@@ -0,0 +1,64 @@
+{pkgs, ...}: {
+  services = {
+    udiskie.enable = true;
+    gpg-agent = {
+      enable = true;
+      pinentryFlavor = "gnome3";
+      enableSshSupport = true;
+      enableZshIntegration = true;
+    };
+  };
+  programs = {
+    gpg.enable = true;
+    man.enable = true;
+    eza.enable = true;
+    dircolors = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    skim = {
+      enable = true;
+      enableZshIntegration = true;
+      defaultCommand = "rg --files --hidden";
+      changeDirWidgetOptions = [
+        "--preview 'eza --icons --git --color always -T -L 3 {} | head -200'"
+        "--exact"
+      ];
+    };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    tealdeer = {
+      enable = true;
+      settings = {
+        display = {
+          compact = false;
+          use_pager = true;
+        };
+        updates = {
+          auto_update = true;
+        };
+      };
+    };
+    bat = {
+      enable = true;
+      config = {
+        pager = "less -FR";
+        theme = "catppuccin-mocha";
+      };
+      themes = {
+        catppuccin-mocha = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-mocha.tmTheme";
+        };
+      };
+    };
+  };
+}
