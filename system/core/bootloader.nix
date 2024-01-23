6e5adaedd88a14d297190286c891b6f1129d09a3
sioodmy <hello@sioodmy.dev>
2024-01-23 21:16:18 +0100

    feat: switch to vanilla kernel
    Former-commit-id: d6ea9f435ad073df8b59ef1fe8b1209e48634ddc

--- system/core/bootloader.nix
+++ system/core/bootloader.nix
@@ -18,8 +18,7 @@ in {
     # some kernel parameters, i dont remember what half of this shit does but who cares
     consoleLogLevel = mkDefault 0;
     initrd.verbose = false;
-    # switch from old ass lts kernel
-    kernelPackages = mkDefault pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = mkDefault pkgs.linuxPackages_latest;
     kernelParams = [
       "psmouse.synaptics_intertouch=1"
     ];
@@ -27,6 +26,7 @@ in {
     bootspec.enable = mkDefault true;
     loader = {
       systemd-boot.enable = mkDefault true;
+      # spam space to get to boot menu
       timeout = 0;
     };
     loader.efi.canTouchEfiVariables = true;
