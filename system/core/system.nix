381ea88a1b35a78752572451ae0e3ff38280dc04
sioodmy <hello@sioodmy.dev>
2024-03-23 17:12:22 +0100

    fix: deprecated stuff

--- system/core/system.nix
+++ system/core/system.nix
@@ -110,9 +110,6 @@
   };
 
   programs = {
-    # type "fuck" to fix the last command that made you go "fuck"
-    thefuck.enable = true;
-
     # allow users to mount fuse filesystems with allow_other
     fuse.userAllowOther = true;
 
