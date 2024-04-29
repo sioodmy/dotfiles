43706398959af57ad47b3b351eae36a8cd3fe668
sioodmy <hello@sioodmy.dev>
2024-04-29 21:28:24 +0200

    feat: server config
    Former-commit-id: 08a733d9b4a71c7beefbccec8553e8c7b0c56989

--- system/server/default.nix
+++ system/server/default.nix
@@ -8,43 +8,26 @@
 imports = [inputs.schizosearch.nixosModules.default];
   services.nginx = {
     enable = true;
-    package = pkgs.nginx.override {openssl = pkgs.libressl;};
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedProxySettings = true;
-    recommendedZstdSettings = true;
-    # FIXME: this normally makes the /nginx_status endpoint availabe, but nextcloud hijacks it and returns a SSL error
-    # we need it for prometheus, so it would be *great* to figure out a solution
-    statusPage = false;
+    # package = pkgs.nginx.override {openssl = pkgs.libressl;};
+    package = pkgs.angieQuic;
 
-    # lets be more picky on our ciphers and protocols
-    sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
-    sslProtocols = "TLSv1.3 TLSv1.2";
+    # # lets be more picky on our ciphers and protocols
+    # sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
+    # sslProtocols = "TLSv1.3 TLSv1.2";
 
-    commonHttpConfig = ''
-      #real_ip_header CF-Connecting-IP;
-      add_header 'Referrer-Policy' 'origin-when-cross-origin';
-      add_header X-Frame-Options DENY;
-      add_header X-Content-Type-Options nosniff;
-    '';
-     virtualHosts."search.sioodmy.dev" = {
-      locations."/".proxyPass = "http://0.0.0.0:3000";
-      extraConfig = ''
-        access_log /dev/null;
-        error_log /dev/null;
-        proxy_connect_timeout 60s;
-        proxy_send_timeout 60s;
-        proxy_read_timeout 60s;
-      '';
-      addSSL = true;
-      enableACME = true;
-    };
-    virtualHosts = {
-      "sioodmy.dev" = {
-        root = inputs.sioodmy-dev.packages.${pkgs.system}.website;
-        enableACME = true;
+     virtualHosts = {
+      "sioodmy.dev" = {          
+          root = "${inputs.sioodmy-dev.packages.${pkgs.system}.website}/";
+          enableACME = true;
+          forceSSL = true;
+      };
+      "search.sioodmy.dev" = {
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+        };
+        quic = true;
         forceSSL = true;
+        enableACME = true;
       };
     };
   };
