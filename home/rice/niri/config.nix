1531735be541688a9d52e20463eec0c92be77f9d
sioodmy <hello@sioodmy.dev>
2024-05-15 20:45:19 +0200

    feat: swaylock

--- home/rice/niri/config.nix
+++ home/rice/niri/config.nix
@@ -103,6 +103,7 @@ with theme.colors; {
       "Mod+Space".action = spawn "${pkgs.fuzzel}/bin/fuzzel";
       "Mod+V".action = sh "${pkgs.cliphist}/bin/cliphist list | fuzzel --dmenu | cliphist decode | wl-copy";
       "Mod+Shift+Period".action = spawn "emoji";
+      "Mod+Shift+L".action = spawn "swaylock" "--grace" "0";
 
       "XF86AudioRaiseVolume".action = spawn "pamixer" "-i" "5";
       "XF86AudioLowerVolume".action = spawn "pamixer" "-d" "5";
