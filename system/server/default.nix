b7c3f3cb37ed51834157b90b27f9a8ccacf232eb
sioodmy <hello@sioodmy.dev>
2024-05-30 10:41:55 +0200

    chore: fmt
    Former-commit-id: 938d3c0087d806cedfce46ddfde17b9869b8badf

--- system/server/default.nix
+++ system/server/default.nix
@@ -3,77 +3,76 @@
   inputs,
   config,
   ...
-}:
-let
+}: let
   inherit (config.age) secrets;
-  in
-# TODO
-{
-  imports = [
-    ./mail.nix
-    inputs.lyricsapi.nixosModules.default
-    inputs.bitcoinstatus.nixosModules.default
-  ];
-  services.nginx = {
-    enable = true;
-    # package = pkgs.nginx.override {openssl = pkgs.libressl;};
-    package = pkgs.angieQuic.override {openssl = pkgs.libressl;};
+in
+  # TODO
+  {
+    imports = [
+      ./mail.nix
+      inputs.lyricsapi.nixosModules.default
+      inputs.bitcoinstatus.nixosModules.default
+    ];
+    services.nginx = {
+      enable = true;
+      # package = pkgs.nginx.override {openssl = pkgs.libressl;};
+      package = pkgs.angieQuic.override {openssl = pkgs.libressl;};
 
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedProxySettings = true;
-    recommendedZstdSettings = true;
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      recommendedGzipSettings = true;
+      recommendedProxySettings = true;
+      recommendedZstdSettings = true;
 
-    # lets be more picky on our ciphers and protocols
-    sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
-    sslProtocols = "TLSv1.3 TLSv1.2";
+      # lets be more picky on our ciphers and protocols
+      sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
+      sslProtocols = "TLSv1.3 TLSv1.2";
 
-    commonHttpConfig = ''
-      #real_ip_header CF-Connecting-IP;
-      add_header 'Referrer-Policy' 'origin-when-cross-origin';
-      add_header X-Frame-Options DENY;
-      add_header X-Content-Type-Options nosniff;
-    '';
+      commonHttpConfig = ''
+        #real_ip_header CF-Connecting-IP;
+        add_header 'Referrer-Policy' 'origin-when-cross-origin';
+        add_header X-Frame-Options DENY;
+        add_header X-Content-Type-Options nosniff;
+      '';
 
-    virtualHosts = {
-      "sioodmy.dev" = {
-        root = inputs.website.packages.${pkgs.system}.website;
-        enableACME = true;
-        locations."/" = {
-          tryFiles = "$uri/index.html $uri.html $uri/ $uri =404";
-          extraConfig = ''
-            rewrite ^(/.*)\.html(\?.*)?$ $1$2 permanent;
-            rewrite ^/(.*)/$ /$1 permanent;
+      virtualHosts = {
+        "sioodmy.dev" = {
+          root = inputs.website.packages.${pkgs.system}.website;
+          enableACME = true;
+          locations."/" = {
+            tryFiles = "$uri/index.html $uri.html $uri/ $uri =404";
+            extraConfig = ''
+              rewrite ^(/.*)\.html(\?.*)?$ $1$2 permanent;
+              rewrite ^/(.*)/$ /$1 permanent;
 
-            error_page 404 /404.html;
-          '';
+              error_page 404 /404.html;
+            '';
+          };
+
+          forceSSL = true;
         };
-
-        forceSSL = true;
-      };
-      "lyrics.sioodmy.dev" = {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:3000";
+        "lyrics.sioodmy.dev" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3000";
+          };
+          quic = true;
+          forceSSL = true;
+          enableACME = true;
         };
-        quic = true;
-        forceSSL = true;
-        enableACME = true;
       };
     };
-  };
-  services.lyricsapi.enable = true;
-  services.bitcoinstatus = {
-    enable = true;
-    tokenFile = secrets.discordtoken.path;
-  };
+    services.lyricsapi.enable = true;
+    services.bitcoinstatus = {
+      enable = true;
+      tokenFile = secrets.discordtoken.path;
+    };
 
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "hello@sioodmy.dev";
-  };
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "hello@sioodmy.dev";
+    };
 
-  networking.firewall = {
-    allowedTCPPorts = [80 443];
-  };
-}
+    networking.firewall = {
+      allowedTCPPorts = [80 443];
+    };
+  }
