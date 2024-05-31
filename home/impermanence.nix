47c54eb979881ac48a4b5b4969caef720872fbdf
sioodmy <hello@sioodmy.dev>
2024-05-31 15:44:17 +0200

    feat: nicotine

--- home/impermanence.nix
+++ home/impermanence.nix
@@ -13,13 +13,13 @@ in {
         "vids"
         "other"
       ]
-      ++ forEach ["syncthing" "Caprine" "VencordDesktop" "obs-studio" "Signal" "niri" "BraveSoftware"] (
+      ++ forEach ["syncthing" "Caprine" "VencordDesktop" "obs-studio" "Signal" "niri" "BraveSoftware" "nicotine"] (
         x: ".config/${x}"
       )
       ++ forEach ["tealdeer" "keepassxc" "nix" "starship" "nix-index" "mozilla" "go-build" "BraveSoftware"] (
         x: ".cache/${x}"
       )
-      ++ forEach ["direnv" "TelegramDesktop" "PrismLauncher" "keyrings"] (x: ".local/share/${x}")
+      ++ forEach ["direnv" "TelegramDesktop" "PrismLauncher" "keyrings" "nicotine"] (x: ".local/share/${x}")
       ++ [".ssh" ".keepass" ".mozilla" ".thunderbird"];
   };
 }
