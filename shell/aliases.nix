9beee7cd46e888f30f5b745978ab08a4d3e9223a
sioodmy <hello@sioodmy.dev>
2024-08-11 19:31:13 +0200

    fix: minor env variable changes
    Former-commit-id: fe236b143ed7450a23066be9b4373dbf50adcb72

--- shell/aliases.nix
+++ shell/aliases.nix
@@ -1,33 +1,30 @@
 {pkgs, ...}: let
-  getExe = pkgs.lib.getExe;
-in
-  with pkgs; rec {
-    l = "${getExe eza} --icons";
-    e = "${getExe eza} --icons -lha --git";
+  inherit (pkgs.lib) getExe;
+in rec {
+  l = "${getExe pkgs.eza} --icons";
+  e = "${getExe pkgs.eza} --icons -lha --git";
 
-    ls = l;
-    la = e;
+  ls = l;
+  la = e;
 
-    m = "mkdir";
+  m = "mkdir";
 
-    g = "git";
-    n = "nix";
+  g = "git";
+  n = "nix";
+  v = "nvim";
 
-    ytmp3 = "yt-dlp -x --embed-metadata --audio-quality 0 --audio-format mp3";
+  ytmp3 = "yt-dlp -x --embed-metadata --audio-quality 0 --audio-format mp3";
 
-    cat = "${getExe bat}";
-    fzf = "${getExe skim}";
+  cat = "${getExe pkgs.bat}";
+  fzf = "${getExe pkgs.skim}";
 
-    cp = "cp -ivr";
-    mv = "mv -iv";
+  kys = "shutdown now";
 
-    kys = "shutdown now";
+  gpl = "curl https://www.gnu.org/licenses/gpl-3.0.txt -o LICENSE";
 
-    gpl = "curl https://www.gnu.org/licenses/gpl-3.0.txt -o LICENSE";
-
-    gcb = "git checkout";
-    gd = "git pull";
-    gu = "git push";
-    gc = "git commit";
-    ga = "git add";
-  }
+  gcb = "git checkout";
+  gd = "git pull";
+  gu = "git push";
+  gc = "git commit";
+  ga = "git add";
+}
