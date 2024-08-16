6b87fda76d487f69f061f794b58940e8f274b062
sioodmy <hello@sioodmy.dev>
2024-08-16 14:27:23 +0200

    chore: bump inputs
    Former-commit-id: 2a36296dce7aa3403482b914faed173801a5a832

--- system/wayland/desktop/wrapped/foot/default.nix
+++ system/wayland/desktop/wrapped/foot/default.nix
@@ -4,83 +4,106 @@
   colors,
   ...
 }: let
-  config = with colors;
-    pkgs.writeText "foot.ini" ''
-      [bell]
-      command=notify-send bell
-      command-focused=no
-      notify=yes
-      urgent=yes
+  inherit (pkgs.lib.meta) getExe getExe';
 
-      [colors]
-      alpha=1.0
-      background=${base00}
-      bright0=${base03}
-      bright1=${base08}
-      bright2=${base0B}
-      bright3=${base0A}
-      bright4=${base0D}
-      bright5=${base0E}
-      bright6=${base0C}
-      bright7=${base07}
-      foreground=${base05}
-      regular0=${base00}
-      regular1=${base08}
-      regular2=${base0B}
-      regular3=${base0A}
-      regular4=${base0D}
-      regular5=${base0E}
-      regular6=${base0C}
-      regular7=${base05}
+  notify-send = getExe pkgs.libnotify;
+  xdg-open = getExe' pkgs.xdg-utils "xdg-open";
 
-      [cursor]
-      beam-thickness=2
-      style=beam
+  config = pkgs.writeText "foot.ini" (pkgs.lib.generators.toINI {} {
+    main = {
+      term = "foot";
+      app-id = "foot";
+      title = "foot";
+      locked-title = "no";
 
-      [key-bindings]
-      show-urls-launch=Control+Shift+u
-      unicode-input=Control+Shift+i
+      font = "monospace:size=16";
+      line-height = 20;
+      letter-spacing = 0;
+      horizontal-letter-offset = 0;
+      vertical-letter-offset = -0.75;
+      box-drawings-uses-font-glyphs = "no";
+      dpi-aware = "no";
 
-      [main]
-      app-id=foot
-      bold-text-in-bright=no
-      dpi-aware=yes
-      font=monospace:size=9
-      locked-title=no
-      notify=notify-send -a $/{app-id} -i $/{app-id} $/{title} $/{body}
-      pad=12x21 center
-      resize-delay-ms=100
-      selection-target=primary
-      term=xterm-256color
-      title=foot
-      vertical-letter-offset=-0.75
-      word-delimiters=,│`|:"'()[]{}<>
+      initial-window-size-chars = "104x36";
+      initial-window-mode = "windowed";
+      pad = "5x5 center";
+      resize-delay-ms = 100;
 
-      [mouse]
-      hide-when-typing=yes
-
-      [mouse-bindings]
-      primary-paste=BTN_MIDDLE
-      select-begin=BTN_LEFT
-      select-begin-block=Control+BTN_LEFT
-      select-extend=BTN_RIGHT
-      select-extend-character-wise=Control+BTN_RIGHT
-      select-word=BTN_LEFT-2
-      select-word-whitespace=Control+BTN_LEFT-2
-      selection-override-modifiers=Shift
-
-      [scrollback]
-      lines=10000
-      multiplier=3
-
-      [url]
-      label-letters=sadfjklewcmpgh
-      launch=xdg-open $\{url}
-      osc8-underline=url-mode
-      protocols=http, https, ftp, ftps, file, gemini, gopher, irc, ircs
-      uri-characters=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,~:;/?#@!$&%*+="'()[]
-
-    '';
+      bold-text-in-bright = "no";
+      word-delimiters = ",│`|:\"'()[]{}<>";
+      selection-target = "primary";
+    };
+    bell = {
+      urgent = "yes";
+      notify = "yes";
+      command = "${notify-send} bell";
+      command-focused = "no";
+    };
+    scrollback = {
+      lines = 100000;
+      multiplier = 10.0;
+      indicator-position = "relative";
+      indicator-format = "line";
+    };
+    url = {
+      launch = "${xdg-open} \${url}";
+      label-letters = "sadfjklewcmpgh";
+      osc8-underline = "always";
+      protocols = "http, https, ftp, ftps, file, gemini, gopher, irc, ircs";
+      uri-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,~:;/?#@!$&%*+=\"'()[]";
+    };
+    cursor = {
+      style = "block";
+      blink = "no";
+    };
+    mouse = {
+      hide-when-typing = "yes";
+      alternate-scroll-mode = "yes";
+    };
+    csd = {
+      preferred = "server";
+    };
+    key-bindings = {
+      scrollback-up-half-page = "Control+k";
+      scrollback-up-page = "Control+Shift+k";
+      scrollback-down-half-page = "Control+j";
+      scrollback-down-page = "Control+Shift+j";
+    };
+    mouse-bindings = {
+      selection-override-modifiers = "Shift";
+      primary-paste = "BTN_MIDDLE";
+      select-begin = "BTN_LEFT";
+      select-begin-block = "Control+BTN_LEFT";
+      select-extend = "BTN_RIGHT";
+      select-extend-character-wise = "Control+BTN_RIGHT";
+      select-word = "BTN_LEFT-2";
+      select-word-whitespace = "Control+BTN_LEFT-2";
+    };
+    desktop-notifications = {
+      command = "${notify-send} -a \${app-id} -i \${app-id} \${title} \${body}";
+    };
+    colors = with colors; {
+      alpha = 1.0;
+      background = "${base00}";
+      bright0 = "${base03}";
+      bright1 = "${base08}";
+      bright2 = "${base0B}";
+      bright3 = "${base0A}";
+      bright4 = "${base0D}";
+      bright5 = "${base0E}";
+      bright6 = "${base0C}";
+      bright7 = "${base07}";
+      foreground = "${base05}";
+      regular0 = "${base00}";
+      regular1 = "${base08}";
+      regular2 = "${base0B}";
+      regular3 = "${base0A}";
+      regular4 = "${base0D}";
+      regular5 = "${base0E}";
+      regular6 = "${base0C}";
+      regular7 = "${base05}";
+    };
+  });
 in {
   basePackage = pkgs.foot;
   flags = ["--config=${config}"];
