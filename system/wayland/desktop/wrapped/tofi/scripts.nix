e67eec073850a768e525f0ddce9c4fc3f00feb2b
sioodmy <hello@sioodmy.dev>
2024-08-17 15:24:14 +0200

    feat: tofi improvments
    Former-commit-id: be0fa79e2ae6c5b6f6ff2765703260fb66223f41

---
+++ system/wayland/desktop/wrapped/tofi/scripts.nix
@@ -0,0 +1,65 @@
+{pkgs, ...}: let
+  inherit (pkgs.lib.meta) getExe' getExe;
+
+  mkTofi = {
+    name,
+    script,
+    makeDesktop ? false,
+  }: let
+    script-name = "tofi-${name}";
+    script-out = pkgs.writeShellScriptBin "${script-name}" script;
+  in
+    if makeDesktop
+    then
+      pkgs.symlinkJoin {
+        inherit name;
+        paths = [
+          (pkgs.makeDesktopItem {
+            name = script-name;
+            desktopName = script-name;
+            exec = "${getExe script-out}";
+          })
+          script-out
+        ];
+      }
+    else script-out;
+in [
+  (
+    mkTofi {
+      name = "calc";
+      script = ''
+        #!/bin/sh
+        set -e
+
+        INPUT=$(echo | tofi --require-match false --prompt-text "calc: " --height 80)
+        OUTPUT="$(echo "scale=2; $INPUT" | ${getExe' pkgs.bc "bc"} -s)"
+
+        notify-send "$OUTPUT" "$INPUT"
+      '';
+      makeDesktop = true;
+    }
+  )
+
+  (
+    mkTofi {
+      name = "confirm";
+      script = ''
+        #!/bin/sh
+
+        # usage: tofi-confirm [PROMPT TEXT] [SHELL COMMAND TO EXECUTE]
+        [[ "$(printf 'No\nYes' | tofi --prompt-text "$1")" == "Yes" ]] && exec sh -c "$2"
+      '';
+    }
+  )
+
+  (
+    mkTofi {
+      name = "emoji";
+      script = ''
+        #!/bin/sh
+        cat ${./emojis} | tofi | awk '{print $1}' | tr -d '\n' | tee >(${getExe' pkgs.wl-clipboard "wl-copy"}) >(xargs -I % notify-send "% Emoji" "Emoji copied to clipboard")
+      '';
+      makeDesktop = true;
+    }
+  )
+]
