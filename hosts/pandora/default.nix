ac10e7ed62672dce0251098fa4589997f873e7b7
sioodmy <hello@sioodmy.dev>
2024-11-09 22:22:04 +0100

    feat: boot options
    Former-commit-id: a0a970fbee6c54eaaf0f00c31b2213b6025e7ef1

--- hosts/pandora/default.nix
+++ hosts/pandora/default.nix
@@ -10,30 +10,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.kernelPatches = [
-    {
-      name = "edge-config";
-      patch = null;
-      # derived from
-      # https://github.com/AsahiLinux/PKGBUILDs/blob/stable/linux-asahi/config.edge
-      extraConfig = ''
-        DRM_SIMPLEDRM_BACKLIGHT n
-        BACKLIGHT_GPIO n
-        DRM_APPLE m
-        APPLE_SMC m
-        APPLE_SMC_RTKIT m
-        APPLE_RTKIT m
-        APPLE_MBOX m
-        GPIO_MACSMC m
-        DRM_VGEM n
-        DRM_SCHED y
-        DRM_GEM_SHMEM_HELPER y
-        DRM_ASAHI m
-        SUSPEND y
-      '';
-    }
-  ];
-
   hardware.asahi = {
     enable = true;
     extractPeripheralFirmware = true;
@@ -43,8 +19,36 @@
     experimentalGPUInstallMode = "replace";
   };
 
-  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = lib.mkForce false;
+    };
+    kernelPatches = [
+      {
+        name = "edge-config";
+        patch = null;
+        # derived from
+        # https://github.com/AsahiLinux/PKGBUILDs/blob/stable/linux-asahi/config.edge
+        extraConfig = ''
+          DRM_SIMPLEDRM_BACKLIGHT n
+          BACKLIGHT_GPIO n
+          DRM_APPLE m
+          APPLE_SMC m
+          APPLE_SMC_RTKIT m
+          APPLE_RTKIT m
+          APPLE_MBOX m
+          GPIO_MACSMC m
+          DRM_VGEM n
+          DRM_SCHED y
+          DRM_GEM_SHMEM_HELPER y
+          DRM_ASAHI m
+          SUSPEND y
+        '';
+      }
+    ];
 
-  boot.initrd.availableKernelModules = ["usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["usbhid" "dm-snapshot"];
+    initrd.availableKernelModules = ["usbhid" "usb_storage" "sd_mod"];
+    initrd.kernelModules = ["usbhid" "dm-snapshot"];
+  };
 }
