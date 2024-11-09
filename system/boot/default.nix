cf9bf15cd51ce5d06ed2d3b617b85e394ff58301
sioodmy <hello@sioodmy.dev>
2024-11-09 22:27:15 +0100

    feat: boot partition
    Former-commit-id: 63117f917905ad8c1e1a3e044af71878952e5277

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -4,6 +4,14 @@
       verbose = false;
       systemd.enable = true;
     };
-    loader.systemd-boot.configurationLimit = 5;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;
+      };
+      editor = false;
+      # spam space to get to boot menu
+      timeout = 0;
+    };
   };
 }
