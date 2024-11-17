2dc551390aa6711d2bec817ddbd34ef664d784f2
sioodmy <hello@sioodmy.dev>
2024-11-17 16:26:17 +0100

    feat: move bluetooth out of the laptop module

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -49,10 +49,6 @@ in {
         };
       };
     };
-    hardware.bluetooth = {
-      enable = true;
-      package = pkgs.bluez5-experimental;
-    };
     # https://github.com/NixOS/nixpkgs/issues/114222
     systemd.user.services.telephony_client.enable = false;
 
