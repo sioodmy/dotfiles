84f2b823ff0fe7b3c87219ecacc104fabe10d471
sioodmy <hello@sioodmy.dev>
2024-11-26 14:59:23 +0100

    feat: remove flake-parts
    Former-commit-id: 38df86a25174bed9613d20aa5154928787da9702

--- flake.nix
+++ flake.nix
@@ -1,39 +1,53 @@
 {
   description = "Consequence of allowing autistic people on the internet. Stay mad one-proper-config-structure purists :3";
 
-  outputs = inputs @ {flake-parts, ...}: let
+  outputs = inputs @ {nixpkgs, ...}: let
     theme = import ./theme;
     user = import ./user theme;
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      flake = {
-        nixosModules =
-          {
-            # This module is not meant to be imported by anyone but me
-            # it's just so I can easily avoid ../../../../../ mess
-            system = import ./system;
 
-            user = user.module;
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        user.packages pkgs
+    );
 
-            # place for my home brew modules
-          }
-          // import ./modules;
-        nixosConfigurations = import ./hosts inputs;
-      };
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      perSystem = {pkgs, ...}: {
-        formatter = pkgs.alejandra;
-        packages = user.packages pkgs;
-        devShells.default = user.shell pkgs;
-      };
-    };
+    formatter = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        pkgs.alejandra
+    );
+
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {default = user.shell pkgs;}
+    );
+    nixosModules =
+      {
+        # This module is not meant to be imported by anyone but me
+        # it's just so I can easily avoid ../../../../../ mess
+        system = import ./system;
+
+        user = user.module;
+
+        # place for my home brew modules
+      }
+      // import ./modules;
+
+    nixosConfigurations = import ./hosts inputs;
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     apple-silicon-support = {
       url = "github:tpwrules/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
