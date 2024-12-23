3e90fec5f9d6dd5ba47c868d1b6aa4a1b504cddd
sioodmy <hello@sioodmy.dev>
2024-12-24 00:10:18 +0100

    fix: disable smooth brightness
    Former-commit-id: 44b5c95c46ff72b5e917a340770fafa4b9ba2445

--- user/wrapped/hypr/configs/Hyprland.nix
+++ user/wrapped/hypr/configs/Hyprland.nix
@@ -131,8 +131,8 @@ in {
     ", XF86AudioLowerVolume, exec, wpctl set-volume -l '1.0' @DEFAULT_AUDIO_SINK@ 6%-"
 
     # backlight
-    ", XF86MonBrightnessUp, exec, brillo -q -u 300000 -A 5"
-    ", XF86MonBrightnessDown, exec, brillo -q -u 300000 -U 5"
+    ", XF86MonBrightnessUp, exec, brillo -q -A 5"
+    ", XF86MonBrightnessDown, exec, brillo -q -U 5"
   ];
   bind =
     [
